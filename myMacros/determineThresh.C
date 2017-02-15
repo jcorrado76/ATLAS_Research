9e98d37cb6a5b31d44bb2e9005b4683bd48a3f88
Joseph Corrado <josephcorrado7@gmail.com>
2017-02-15 03:03:33 -0500

    Mincer Meeting 2/15
    Determined Thresholds to keep 10^(-4) events

--- myMacros/determineThresh.C
+++ myMacros/determineThresh.C
@@ -1,25 +1,13 @@
-{
-	/*
-	Joseph Corrado plotEffiency.c
-	Plots effiencies of various ATLAS algorithms versus the MET OFFLINE RECAL algorithm
-	Plot cuts only bounded below
-	*/
-
-	//# ALGS
-	int N = 7;
-	//ALGS
-	TString METalgArray[N] = { "metl1","metcell","metmht","mettopocl","mettopoclps","mettopoclpuc","passrndm" };
-	TString SETalgArray[N] = {};
-
-	//arbitrary cut values
-	Float_t cutGuess = 80;
-
+int determineThresh(TString& alg, double initialGuess = 50.) {
 	//ROOT FileName
-	TString fileName = "ZeroBias2016.13Runs.root";
+	TString fileName = "../myData/ZeroBias2016.13Runs.root";
+
+	TString nameArray[6] = { "metl1","metcell","metmht","mettopocl","mettopoclps","mettopoclpuc" };
+
 
 	//Passed hist parameters
-	TString passed_hist_name = "mettopocl";
-	TString passed_hist_title = "METTOPOCL";
+	TString passed_hist_name = alg;
+	TString passed_hist_title = alg;
 
 	//Reference hist parameters 
 	TString reference_hist_name = "metoffrecal";
@@ -28,83 +16,76 @@
 	//Histogram parameters
 	int nbins = 250;
 	Double_t metMin = 0.0;
-	Double_t metMax = 100.0;
+	Double_t metMax = 200.0;
 
 	//Histogram Axis labels 
 	TString xaxis = "MET [GeV]";
 	TString yaxis = "Events";
 
+	TString teff_name, teff_title;
+
 	//Reference Hist Labels
 	TString reference_hist_full_param = (reference_hist_title + " HIST;" + xaxis + ";" + yaxis);
 
 	//Calculate Full Hist Params
-	TString passed_hist_full_param1 = (passed_hist_title + Form(" > %.2f", cutGuess) + " HIST; " + xaxis + ";" + yaxis);
-
-
-
-	//========================================================================================================================
-	//TEfficiency Labels
-	TString teff_name, teff_title1, teff_title2, teff_title3;
-	teff_name = passed_hist_name + " vs " + reference_hist_name;
-	teff_title1 = passed_hist_title + " / " + reference_hist_title + " , " + Form(" > %.2f", cutGuess) + " Efficiency" + ";" + xaxis + ";" + " Efficiency";
+	TString passed_hist_full_param1 = (passed_hist_title + Form(" > %.2f", initialGuess) + " HIST; " + xaxis + ";" + yaxis);
+	
 
 	//Open ROOT file 
 	TFile * 2016Data = TFile::Open(fileName, "READ");
 
-	//containers for 1st entry of met 
+
+	//variables take on values of alg
 	Float_t passed_hist_met, reference_hist_met;
 
 	//Get addresses of variables
+	tree->SetBranchAddress(passed_hist_name, &passed_hist_met);
 	tree->SetBranchAddress(reference_hist_name, &reference_hist_met);
 
-	//Initial TH1F objects
+	//Initial TH1F object
 	TH1F *cut = new TH1F(passed_hist_name, passed_hist_full_param1, nbins, metMin, metMax);
 
 
-	//Initialize TEfficiency Objects 
-	TEfficiency *teff = new TEfficiency(teff_name, teff_title1, nbins, metMin, metMax);
 
-	//Fill hist and effiency with entries
-	Int_t entries = 100;
-	bool pass = false;
 	//# of entries
 	int nentries = tree->GetEntries();
 
-	//for each algorithm
-	for (Int_t i = 0; i < N; i++)
+	double condition = (10 * *(-4))*nentries;
+
+	for (Long64_t j = 0; j < nentries; j++)
 	{
-		passed_hist_name = METalgArray[i];
-		tree->SetBranchAddress(passed_hist_name, &passed_hist_met);
-		//input all entries first time
-		for (Int_t j = 0; j < nentries; j++)
+		//get the first entry; after adding, get next entry
+		tree->GetEntry(j);
+		//If the passed_hist_met is above a certain value, add the reference_hist_met from the corresponding entry 
+		if (passed_hist_met > initialGuess)
 		{
-			tree->GetEntry(j);
-			//If the passed_hist_met is above a certain value, add the reference_hist_met from the corresponding entry 
-			if (passed_hist_met > cutGuess)
-			{
-				pass = true;
-				cut->Fill(passed_hist_met);
-				//teff->Fill(pass, reference_hist_met);
-			}
+			cut->Fill(passed_hist_met);
 		}
-		//if cut size is too large, create new hist and try again 
-		while (cut->GetSize() > (nentries * (0.0001)))
-		{
-			TH1F *cut = new TH1F(passed_hist_name + "hist", passed_hist_full_param1, nbins, metMin, metMax);
-			for (j = 0; j < nentries; j++)
-			{
-				tree->GetEntry(j);
-				//If the passed_hist_met is above a certain value, add the reference_hist_met from the corresponding entry 
-				if (passed_hist_met > cutGuess)
-				{
-					pass = true;
-					cut->Fill(passed_hist_met);
-					//teff->Fill(pass, reference_hist_met);
-				}
-			}
-			cutGuess++;
-		}
-		std::cout << passed_hist_name << "Cut to keep O(100) events: " << cutGuess << std::endl;
 	}
 
+	int eventsKept  = cut->GetEntries();
+
+	while (eventsKept < condition)
+	{
+		initialGuess = initialGuess +  2.0;
+		for (Long64_t j = 0; j < nentries; j++)
+		{
+			//get the first entry; after adding, get next entry
+			tree->GetEntry(j);
+			//If the passed_hist_met is above a certain value, add the reference_hist_met from the corresponding entry 
+			if (passed_hist_met > initialGuess)
+			{
+				cut->Fill(passed_hist_met);
+			}
+		}
+		eventsKept = cut->GetEntries();
+	}
+
+	std::cout << "Cut to keep 10^(-4) events:  " << initialGuess << std::endl;
+
+	
+
+	cut->Draw();
+
+	return 0;
 }
--- myMacros/plotACut.c
+++ myMacros/plotACut.c
@@ -1,10 +1,13 @@
-{
+int plotACut(TString& alg, double bound){
 	//ROOT FileName
 	TString fileName = "../myData/ZeroBias2016.13Runs.root";
 
+	TString nameArray[6] = { "metl1","metcell","metmht","mettopocl","mettopoclps","mettopoclpuc" };
+
+
 	//Passed hist parameters
-	TString passed_hist_name = "mettopocl";
-	TString passed_hist_title = "METTOPOCL";
+	TString passed_hist_name = alg;
+	TString passed_hist_title = alg;
 
 	//Reference hist parameters 
 	TString reference_hist_name = "metoffrecal";
@@ -13,7 +16,7 @@
 	//Histogram parameters
 	int nbins = 250;
 	Double_t metMin = 0.0;
-	Double_t metMax = 100.0;
+	Double_t metMax = 200.0;
 
 	//Histogram Axis labels 
 	TString xaxis = "MET [GeV]";
@@ -22,13 +25,13 @@
 	TString teff_name, teff_title;
 
 	//cut value; GeV
-	Float_t lwrbnd1 = 50.;
+	Float_t cutValue = bound;
 
 	//Reference Hist Labels
 	TString reference_hist_full_param = (reference_hist_title + " HIST;" + xaxis + ";" + yaxis);
 
 	//Calculate Full Hist Params
-	TString passed_hist_full_param1 = (passed_hist_title + Form(" > %.2f", lwrbnd1) + " HIST; " + xaxis + ";" + yaxis);
+	TString passed_hist_full_param1 = (passed_hist_title + Form(" > %.2f", cutValue) + " HIST; " + xaxis + ";" + yaxis);
 
 	//Open ROOT file 
 	TFile * 2016Data = TFile::Open(fileName, "READ");
@@ -51,18 +54,42 @@
 	bool pass = false;
 	//# of entries
 	int nentries = tree->GetEntries();
-	for (Long64_t i = 0; i < nentries; i++)
+	
+	TF1 * myFit = new TF1("myFit", "gaus(0)", 50, 200);
+
+
+	std::cout << "Number of entries " << nentries << std::endl;
+
+	
+	for (Long64_t j = 0; j < nentries; j++)
 	{
 		//get the first entry; after adding, get next entry
-		tree->GetEntry(i);
+		tree->GetEntry(j);
 		//If the passed_hist_met is above a certain value, add the reference_hist_met from the corresponding entry 
-		if (passed_hist_met > lwrbnd1)
+		if (passed_hist_met > cutValue)
 		{
-			pass = true;
+
 			cut->Fill(passed_hist_met);
-			teff->Fill(pass, reference_hist_met);
+			teff->Fill(true, reference_hist_met);
 		}
 	}
+	//Int_t entriesPassed = cut->GetEntries();
+	//Double_t ratioPassed = entriesPassed / nentries;
+	//std::cout << "Ratio of events kept: " << ratioPassed << std::endl;
+	//Double_t secParam = cut->GetMean();
+	//Double_t thirParam = cut->GetRMS();
+	//Double_t firstParam = 1 / (sqrt(2 * TMath::Pi()) * thirParam);
+	//myFit = cut->Fit("gaus");
+
+	//TCanvas *c = new TCanvas;
+	////c->Divide(1, 2)
+	////c->cd(1);
+	////myFit->Draw();
+	////c->cd(2);
 	cut->Draw();
+
+	
+	
 	//teff->Draw();
+	return 0;
 }
