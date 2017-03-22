65f0dfba7d805a2042f0f912bee4e363d2b4095e
Joseph Corrado <josephcorrado7@gmail.com>
2017-03-22 02:31:34 -0400

    Spacing
    Some spacing changes

--- myMacros/doAnalysis.c
+++ myMacros/doAnalysis.c
@@ -3,11 +3,14 @@ int doAnalysis(TString& alg)
 	//This macro will call plot a cut and determine threshold in order to determine the zero bias thresholds and apply
 	//them to muon events to see how many muon events are kept, 
 	//determines thresh from zero bias
+
 	std::cout << "Entering determineThresh.C" << std::endl;
 	TString threshCommand = ".x determineThresh.c(\"" + alg + "\")";
 	Float_t thresh = gROOT->ProcessLine(threshCommand);
 	TString threshVal = Form("%f", thresh);
 	std::cout << "Thresh value: " << threshVal << std::endl;
+
+
 	std::cout << "Entering plotACut.C" << std::endl;
 	TString cutCommand = ".x plotACut.c(\"" + alg + "\"," + threshVal + ",\"muon\")";
 	gROOT->ProcessLine(cutCommand);
