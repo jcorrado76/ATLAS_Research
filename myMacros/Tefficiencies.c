04aed615fc7338f563f81776e91cff21d0b55fdb
Joseph Corrado <josephcorrado7@gmail.com>
2017-04-04 15:54:52 -0400

    Renamed variables properly

--- myMacros/Tefficiencies.c
+++ myMacros/Tefficiencies.c
@@ -44,13 +44,12 @@
 	TString passed_hist_full_param5 = (passed_hist_title5 + Form(" > %.2f", cutValue5) + " HIST; " + xaxis + ";" + yaxis);
 	TString passed_hist_full_param6 = (passed_hist_title6 + Form(" > %.2f", cutValue6) + " HIST; " + xaxis + ";" + yaxis);
 
-	TString name = "TEfficiencies of Algorithms";
-	teff_name1 = name;
-	teff_name2 = name;
-	teff_name3 = name;
-	teff_name4 = name;
-	teff_name5 = name;
-	teff_name6 = name;
+	teff_name1 = "metl1";
+	teff_name2 = "metcell";
+	teff_name3 = "metmht";
+	teff_name4 = "mettopocl";
+	teff_name5 = "mettopoclps";
+	teff_name6 = "mettopoclpuc";
 
 	TString ylabel = "Efficiency";
 	TString xlabel1 = "metoffrecal";
@@ -61,8 +60,8 @@
 	teff_title5 = teff_name5 + ";" + xlabel1 + ";" + ylabel + ";";
 	teff_title6 = teff_name6 + ";" + xlabel1 + ";" + ylabel + ";";
 
-	TString xlabel2 = "MET No Mu";
-	
+	TString xlabel2 = "MET Offrecal Muon";
+
 	teff_title11 = teff_name1 + ";" + xlabel2 + ";" + ylabel + ";";
 	teff_title22 = teff_name2 + ";" + xlabel2 + ";" + ylabel + ";";
 	teff_title33 = teff_name3 + ";" + xlabel2 + ";" + ylabel + ";";
@@ -100,65 +99,65 @@
 	tree->SetBranchAddress(alg5, &passed_hist_met5);
 	tree->SetBranchAddress(alg6, &passed_hist_met6);
 
+	Float_t mexoffrecal, meyoffrecal, mexoffrecalMuon, meyoffrecalMuon, metoffrecal, metoffrecalmuon;
 
-	TString mexoffrecalName = "mexoffrecal";
-	TString meyoffrecalName = "meyoffrecal";
-	TString mexofflineName = "mexoffrecalmuon";
-	TString meyofflineName = "meyoffrecalmuon";
-
-	Float_t mexoffrecal, meyoffrecal, mexofflineMuon, meyofflineMuon;
-
-	tree->SetBranchAddress(mexoffrecalName, &mexoffrecal);
-	tree->SetBranchAddress(meyoffrecalName, &meyoffrecal);
-	tree->SetBranchAddress(mexofflineName, &mexofflineMuon);
-	tree->SetBranchAddress(meyofflineName, &meyofflineMuon);
+	
 
 	int passmuon;
 	int nentries = tree->GetEntries();
-	Float_t offrecal;
-	tree->SetBranchAddress("passmu24med", &passmuon);
-	tree->SetBranchAddress("metoffrecal", &offrecal);
 
 	std::cout << "Creating TEfficiencies..." << std::endl;
 
+	tree->SetBranchAddress("passmu24med", &passmuon);
+
+	tree->SetBranchAddress("mexoffrecal", &mexoffrecal);
+	tree->SetBranchAddress("meyoffrecal", &meyoffrecal);
+	tree->SetBranchAddress("metoffrecal", &metoffrecal);
+	tree->SetBranchAddress("mexoffrecalmuon", &mexoffrecalMuon);
+	tree->SetBranchAddress("meyoffrecalmuon", &meyoffrecalMuon);
+	tree->SetBranchAddress("metoffrecalmuon", &metoffrecalmuon);
 
 	for (Long64_t j = 0; j < nentries; j++)
 	{
-		Float_t mexnomu = mexoffrecal - mexofflineMuon;
-		Float_t meynomu = meyoffrecal - meyofflineMuon;
+		Float_t mexnomu = mexoffrecal - mexoffrecalMuon;
+		Float_t meynomu = meyoffrecal - meyoffrecalMuon;
+
 		Float_t metnomu = TMath::Power((TMath::Power(mexnomu, 2.0) + TMath::Power(meynomu, 2.0)),0.5); //calculate correct offline value (no mu MET)
 		tree->GetEntry(j);
 
 		if (passmuon == 1)
 		{
-			teff1->Fill((passed_hist_met1 > cutValue1), offrecal);
-			teff2->Fill((passed_hist_met2 > cutValue2), offrecal);
-			teff3->Fill((passed_hist_met3 > cutValue3), offrecal);
-			teff4->Fill((passed_hist_met4 > cutValue4), offrecal);
-			teff5->Fill((passed_hist_met5 > cutValue5), offrecal);
-			teff6->Fill((passed_hist_met6 > cutValue6), offrecal);
+			teff1->Fill((passed_hist_met1 > cutValue1), metoffrecal);
+			teff2->Fill((passed_hist_met2 > cutValue2), metoffrecal);
+			teff3->Fill((passed_hist_met3 > cutValue3), metoffrecal);
+			teff4->Fill((passed_hist_met4 > cutValue4), metoffrecal);
+			teff5->Fill((passed_hist_met5 > cutValue5), metoffrecal);
+			teff6->Fill((passed_hist_met6 > cutValue6), metoffrecal);
 
-			teff11->Fill((passed_hist_met1 > cutValue1), metnomu);
-			teff22->Fill((passed_hist_met2 > cutValue2), metnomu);
-			teff33->Fill((passed_hist_met3 > cutValue3), metnomu);
-			teff44->Fill((passed_hist_met4 > cutValue4), metnomu);
-			teff55->Fill((passed_hist_met5 > cutValue5), metnomu);
-			teff66->Fill((passed_hist_met6 > cutValue6), metnomu);
+			teff11->Fill((passed_hist_met1 > cutValue1), metoffrecalmuon);
+			teff22->Fill((passed_hist_met2 > cutValue2), metoffrecalmuon);
+			teff33->Fill((passed_hist_met3 > cutValue3), metoffrecalmuon);
+			teff44->Fill((passed_hist_met4 > cutValue4), metoffrecalmuon);
+			teff55->Fill((passed_hist_met5 > cutValue5), metoffrecalmuon);
+			teff66->Fill((passed_hist_met6 > cutValue6), metoffrecalmuon);
+		
 		}
 	}
 
-	TCanvas* c1 = new TCanvas("TEfficiencies","TEfficiencies");
+	TCanvas* c1 = new TCanvas("TEfficiencies",title = "TEfficiencies");
 	c1->Divide(2);
 	c1->cd(1);
 	TLegend* leg1 = new TLegend(0.25, 0.5, 0.15, 0.15);
 	leg1->SetFillColor(kWhite);
 	leg1->SetFillStyle(1001);
+	leg1->SetNColumns(2);
 	leg1->AddEntry(teff1, alg1, "L");
 	leg1->AddEntry(teff2, alg2, "L");
 	leg1->AddEntry(teff3, alg3, "L");
 	leg1->AddEntry(teff4, alg4, "L");
 	leg1->AddEntry(teff5, alg5, "L");
 	leg1->AddEntry(teff6, alg6, "L");
+	leg1->SetTextSize(0.05);
 
 	teff1->Draw();
 	teff2->SetLineColor(kBlue);
@@ -179,12 +178,14 @@
 	TLegend* leg = new TLegend(0.25, 0.5, 0.15, 0.15);
 	leg2->SetFillColor(kWhite);
 	leg2->SetFillStyle(1001);
+	leg2->SetNColumns(2);
 	leg2->AddEntry(teff11, alg1, "L");
 	leg2->AddEntry(teff22, alg2, "L");
 	leg2->AddEntry(teff33, alg3, "L");
 	leg2->AddEntry(teff44, alg4, "L");
 	leg2->AddEntry(teff55, alg5, "L");
 	leg2->AddEntry(teff66, alg6, "L");
+	leg2->SetTextSize(0.05);
 
 	teff11->Draw();
 	teff22->SetLineColor(kBlue);
@@ -198,34 +199,21 @@
 	teff66->SetLineColor(kOrange);
 	teff66->Draw("same");
 	leg2->Draw("same");
-	
+
 	TEfficiency* efficiencies[12] = { teff1 ,teff2 ,teff3 ,teff4 ,teff5 ,teff6 ,teff11,teff22,teff33,teff44,teff55,teff66 };
 
-
-	TString singleFileName = "Single_File";
-	TString fileExt = ".pdf";
-	TString targetDirectory("../pictures/");
-
-	TString fullPath(targetDirectory + singleFileName + fileExt);
-
-	myCanv->Print(fullPath + "[");
+	TString path = "../pictures/tefficiencies/Tefficiencies.pdf";
+	TString canvname("");
+	TCanvas* teffCanv = new TCanvas(canvname, "");
+	teffCanv.Print(path+"[");
 	for (int m = 0; m < 12; m++)
-	{
-		//generate canvas name each iteration
-		sprintf(canvname, "canv%d", m);
-		//reassign canvas to new name
-		myCanv = new TCanvas(canvname, "");
-		//put the fit onto the new canvas
-		efficiencies[i]->Draw();
-		//write the new canvas to file 
-		myCanv->Print(fullPath);
+	{	
+		efficiencies[m]->Draw();
+		teffCanv->Print(path);
 	}
-	//close the file ;suppress printing
-	myCanv->Print(fullPath + "]");
+	teffCanv.Print(path + "]");
 
 
-
-	c1->Draw();
 
 	
 }
