32dbe6aca27ea352a06b04ba1d65c327eadd737c
Joseph Corrado <josephcorrado7@gmail.com>
2017-06-08 01:17:34 -0400

    Added L1 cut of 30 GeV everywhere the code determines how many evens kept because the lowest level trigger always fires before the higher level triggers, so I must cut on it anyways when I determine the threshold needed to keep a certain number of events with the higher level algorithms.

--- myMacros/determineCombinedEventsKept.c
+++ myMacros/determineCombinedEventsKept.c
@@ -1,4 +1,4 @@
-Float_t determineCombinedEventsKept(TString& algA, Float_t threshA, TString& algB, Float_t threshB, TString& fileName = "ZeroBias2016new.13Runs.root")
+Float_t determineCombinedEventsKept(TString& algA, Float_t threshA, TString& algB, Float_t threshB, Float_t metl1thresh = 30.0,TString& fileName = "ZeroBias2016new.13Runs.root")
 {
   gROOT->ProcessLine("gROOT->Reset();");
   std::cout << "Determining fraction of zero bias events kept when using combined algorithm of " << algA << " at: " << threshA << " and "
@@ -10,12 +10,14 @@ Float_t determineCombinedEventsKept(TString& algA, Float_t threshA, TString& alg
 	Double_t metMin = 0.0;
 	Double_t metMax = 500.0;
   Int_t nentries = tree->GetEntries();
-  Float_t algAMET, algBMET;
+  Float_t algAMET, algBMET,metl1;
   Int_t passrndm;
+
 
   tree->SetBranchAddress("passrndm",&passrndm);
   tree->SetBranchAddress(algA,&algAMET);
   tree->SetBranchAddress(algB,&algBMET);
+  tree->SetBranchAddress("metl1",&metl1);
 
   Int_t counter,numRndm = 0;
 
@@ -26,7 +28,7 @@ Float_t determineCombinedEventsKept(TString& algA, Float_t threshA, TString& alg
     {
       numRndm++;
     }
-    if ((passrndm > 0.1) && (algAMET > threshA) && (algBMET > threshB))
+    if ((passrndm > 0.1) && (algAMET > threshA) && (algBMET > threshB) && (metl1 > metl1thresh))
     {
       counter++;
     }
