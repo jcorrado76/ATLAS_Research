08c6adefcb85e8a509004c3bcaa5e342a0859750
Joseph Corrado <jgc375@nyu.edu>
2017-06-17 11:05:11 -0400

    made a big macro in order to run generate efficiencies for all 4 combinations of parameters, and for a control group. I had to switch the mechanism from calling groot processline to just compiling the macros and calling as a function

--- myMacros/generateEfficiencies.c
+++ myMacros/generateEfficiencies.c
@@ -1,18 +1,19 @@
-Int_t generateEfficiencies(Float_t metl1Thresh = 30.0, Float_t frac = 1e-4,TString& zeroBiasfileName = "ZeroBias2016R307195R311481Runs56.root",TString& muonFilename = "PhysicsMain2016.Muons.R3073065R311481Runs9.root")
+#include "TString.h"
+#include "TROOT.h"
+
+Int_t generateEfficiencies(Float_t metl1Thresh = 30.0, const Float_t frac = 1e-4, const TString& folderName = "", const TString& zeroBiasFileName = "ZeroBias2016R307195R311481Runs56.root", const TString& muonFilename = "PhysicsMain2016.Muons.noalgL1XE45R3073065R311481Runs9B.root")
 {
   /*
    This program should just call threeefficiencies.c for all of the 15 combinations of algorithms
   */
-  gROOT->ProcessLine(".L threeEfficiencies.c");
+  gROOT->ProcessLine(".L threeEfficiencies.C+");
   TString algArray[6] = {"mettopoclem","metcell","metmht","mettopocl","mettopoclps","mettopoclpuc"};
 //  TString algArray[6] = {"metl1","metcell","metmht","mettopocl","mettopoclps","mettopoclpuc"};
-  TString argc;
   for (Int_t i = 0 ; i < 6 ; i++)
   {
     for (Int_t j = i+1 ; j < 6 ; j++)
     {
-      argc = ".x threeEfficiencies.c(\"" + algArray[i] + "\",\"" + algArray[j] + "\"," + Form("%.7f",metl1thresh) + "," + Form("%.7f",frac) + ",\""+zeroBiasfileName+"\",\""+muonFilename+"\")";
-      gROOT->ProcessLine(argc);
+	threeEfficiencies( algArray[i] , algArray[j] , metl1Thresh , frac , folderName , zeroBiasFileName , muonFilename );
     }
   }
 
