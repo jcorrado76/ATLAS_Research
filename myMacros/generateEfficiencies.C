d106ab72c47ed619d98d1d32cf3dda9854cfd52b
Joseph Corrado <jgc375@nyu.edu>
2017-06-18 01:37:21 -0400

    Edited the argument for gROOT->ProcessLine() so that I could call threeEfficiencies from macros generateEfficiencies and controlgroup. It currently works so that all I have to do is open root and call .x macro

--- myMacros/generateEfficiencies.C
+++ myMacros/generateEfficiencies.C
@@ -6,14 +6,14 @@ Int_t generateEfficiencies(Float_t metl1Thresh = 30.0, const Float_t frac = 1e-4
     /*
     This program should just call threeefficiencies.c for all of the 15 combinations of algorithms
     */
-    gROOT->ProcessLine("gSystem->cd(\"../myMacros\");");
+    gROOT->ProcessLine(".L threeEfficiencies.C");
     TString algArray[6] = {"mettopoclem","metcell","metmht","mettopocl","mettopoclps","mettopoclpuc"};
     //TString algArray[6] = {"metl1","metcell","metmht","mettopocl","mettopoclps","mettopoclpuc"};
     for (Int_t i = 0 ; i < 6 ; i++)
     {
 	for (Int_t j = i+1 ; j < 6 ; j++)
 	{
-	    gROOT->ProcessLine(".x threeEfficiencies.C(\""+ algArray[i]+"\" ,\""+ algArray[j]+" ,"+ Form("%.7f", metl1Thresh)+" , "+Form("%.7f",frac)+" ,\""+ folderName+"\" ,\""+ zeroBiasFileName+"\" ,\""+ muonFilename+"\");" );
+	    gROOT->ProcessLine(".x threeEfficiencies.C(\"" + algArray[i] + "\",\"" + algArray[j] + "\"," + Form("%.7f",metl1Thresh) + "," + Form("%.7f",frac) + ",\""+zeroBiasFileName+"\",\""+muonFilename+"\")");
 	}
     }
 
