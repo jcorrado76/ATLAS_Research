3a888752a0745a53558ddef5cddef47fbac88705
Joseph Corrado <jgc375@nyu.edu>
2017-06-18 00:37:54 -0400

    Tried to make a big macro that runs generateEfficiencies 4 times.

--- myMacros/bigMacro.C
+++ myMacros/bigMacro.C
@@ -1,20 +1,20 @@
 #include "TROOT.h"
-
+#include <iostream>
 Int_t bigMacro()
 {
-    gROOT->ProcessLine("gSystem->cd(\"../myMacros/\"");
-    gROOT->ProcessLine(".L ../myMacros/generateEfficiencies.C+");
+    gROOT->ProcessLine("gSystem->cd(\"../myMacros\")");
+    gROOT->ProcessLine(".L generateEfficiencies.C+;");
     TString algArray[6] = {"mettopoclem","metcell","metmht","mettopocl","mettopoclps","mettopoclpuc"};
-    
-    gROOT->ProcessLine("generateEfficiencies(30,1e-4,\"L130frac1e-4NewestMuonData\"");
-    gROOT->ProcessLine("generateEfficiencies(40,1e-4,\"L140frac1e-4NewestMuonData\"");
-    gROOT->ProcessLine("generateEfficiencies(40,5e-5,\"L140frac5e-5NewestMuonData\"");
-    gROOT->ProcessLine("generateEfficiencies(30,5e-5,\"L130frac5e-5NewestMuonData\"");
- 
+    std::cout << "Inside bigMAcro" << std::endl;
+    gROOT->ProcessLine(".x generateEfficiencies.C(30,1e-4,\"L130frac1e-4NewestMuonData\");");
+    gROOT->ProcessLine("generateEfficiencies(40,1e-4,\"L140frac1e-4NewestMuonData\");");
+    gROOT->ProcessLine("generateEfficiencies(40,5e-5,\"L140frac5e-5NewestMuonData\");");
+    gROOT->ProcessLine("generateEfficiencies(30,5e-5,\"L130frac5e-5NewestMuonData\");");
+    std::cout << "Finished running on all algs" << std::endl;
 
     for (int i = 0 ; i < 6; i++)
     {
-	gROOT->ProcessLine("threeEfficiencies(\" " + algArray[i] +"\" ,\"" +  algArray[i]+ "\" , 30 , 1e-4 , \"controlGroup\" , \"ZeroBias2016R307195R311481Runs56.root\", \"PhysicsMain2016.Muons.noalgL1XE45R3073065R311481Runs9B.root\"" );
+	gROOT->ProcessLine("threeEfficiencies(\" " + algArray[i] +"\" ,\"" +  algArray[i]+ "\" , 30 , 1e-4 , \"controlGroup\" , \"ZeroBias2016R307195R311481Runs56.root\", \"PhysicsMain2016.Muons.noalgL1XE45R3073065R311481Runs9B.root\");" );
     }
 
     return(0);
--- myMacros/generateEfficiencies.C
+++ myMacros/generateEfficiencies.C
@@ -3,19 +3,19 @@
 
 Int_t generateEfficiencies(Float_t metl1Thresh = 30.0, const Float_t frac = 1e-4, const TString& folderName = "", const TString& zeroBiasFileName = "ZeroBias2016R307195R311481Runs56.root", const TString& muonFilename = "PhysicsMain2016.Muons.noalgL1XE45R3073065R311481Runs9B.root")
 {
-  /*
-   This program should just call threeefficiencies.c for all of the 15 combinations of algorithms
-  */
-  gROOT->ProcessLine(".L threeEfficiencies.C+");
-  TString algArray[6] = {"mettopoclem","metcell","metmht","mettopocl","mettopoclps","mettopoclpuc"};
-//  TString algArray[6] = {"metl1","metcell","metmht","mettopocl","mettopoclps","mettopoclpuc"};
-  for (Int_t i = 0 ; i < 6 ; i++)
-  {
-    for (Int_t j = i+1 ; j < 6 ; j++)
+    /*
+    This program should just call threeefficiencies.c for all of the 15 combinations of algorithms
+    */
+    gROOT->ProcessLine("gSystem->cd(\"../myMacros\");");
+    TString algArray[6] = {"mettopoclem","metcell","metmht","mettopocl","mettopoclps","mettopoclpuc"};
+    //TString algArray[6] = {"metl1","metcell","metmht","mettopocl","mettopoclps","mettopoclpuc"};
+    for (Int_t i = 0 ; i < 6 ; i++)
     {
-	gROOT->ProcessLine("threeEfficiencies(\""+ algArray[i]+"\" ,\""+ algArray[j]+" ,"+ Form(%.7f, metl1Thresh)+" , "+Form(%.7f,frac)+" ,\""+ folderName+"\" ,\""+ zeroBiasFileName+"\" ,\""+ muonFilename+"\"" );
+	for (Int_t j = i+1 ; j < 6 ; j++)
+	{
+	    gROOT->ProcessLine(".x threeEfficiencies.C(\""+ algArray[i]+"\" ,\""+ algArray[j]+" ,"+ Form("%.7f", metl1Thresh)+" , "+Form("%.7f",frac)+" ,\""+ folderName+"\" ,\""+ zeroBiasFileName+"\" ,\""+ muonFilename+"\");" );
+	}
     }
-  }
 
   return(0);
 
