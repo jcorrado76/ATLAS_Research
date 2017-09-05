4c40e632630920ec51f003d5b433e4312538bff2
Joseph Corrado <jgc375@nyu.edu>
2017-09-05 00:01:33 -0400

    Started working on macro to simply display the efficiency curves for all of the algorithms together on one plot.

---
+++ myMacros/allEff.C
@@ -0,0 +1,19 @@
+#include "TMath.h"
+#include "TH1.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TString.h"
+#include "TEfficiency.h"
+#include "TLegend.h"
+#include <iostream>
+#include <fstream>
+#include "TROOT.h"
+#include "TCanvas.h"
+#include "TSystem.h"
+#include "TF1.h"
+
+
+Int_t allEff.C()
+{
+    
+}
