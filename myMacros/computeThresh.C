75729714d24ba7c0b4dda8ed8914306368339d6c
Joseph Corrado <jgc375@nyu.edu>
2017-09-18 22:56:48 -0400

    Deleted some more unecessary files, and changed some of the older macros to be updated

--- myMacros/computeThresh.C
+++
@@ -1,9 +0,0 @@
-#include "TH1.h"
-
-Float_t computeThresh(TH1F* target, Float_t numKeep)
-{
-    Int_t nbin = 0;
-    target->GetBinWithContent(numKeep,nbin,0,-1,20000); //if firstx<=0{firstx=1}; if lastx<firstx{lastx=fXaxis.GetNbinsX()};
-    Float_t thresh = (target->GetXaxis())->GetBinCenter(nbin);
-    return(thresh);
-}
