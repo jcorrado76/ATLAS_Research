502783cb1318b8501d9d4dafbdac03bf9fb5c763
Joseph Corrado <jgc375@nyu.edu>
2017-09-20 18:47:57 -0400

    Created macro to compute how many events one would keep using the current trigger configuration being used at CERN, given a new data set

---
+++ myMacros/computeNumbToKeep.C
@@ -0,0 +1,50 @@
+#include "TMath.h"
+#include "TH1.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TString.h"
+#include "TEfficiency.h"
+#include "TLegend.h"
+#include <iostream>
+#include <fstream>
+#include "TROOT.h"
+#include "TCanvas.h"
+#include "TSystem.h"
+#include "TF1.h"
+
+Int_t computeNumbToKeep(const TString& zeroBiasFileName = "PhysicsMain.All.noalgXEtriggers.2016.f731f758._m1659m1710.48Runs.root")
+{
+    /* given a new zerobias file, this macro computes what fraction the triggers currently in use at CERN would keep */
+    TString zerobiasFilePath = "../myData/"+ zerobiasFileName;
+    TFile * zeroBiasFile = TFile::Open(zerobiasFilePath, "READ");
+    TTree* zeroBiasTree = (TTree*)zeroBiasFile->Get("tree");
+    Int_t zerobiasNentries = zeroBiasTree->GetEntries();
+
+    Float_t metl1, metcell;
+    Int_t passnoalgL1XE10 , passnoalgL1XE30, passnoalgL1XE40, passnoalgL1XE45;
+
+    zeroBiasTree->SetBranchAddress("metl1",&metl1);
+    zeroBiasTree->SetBranchAddress("metcell", &metcell)
+    zeroBiasTree->SetBranchAddress("passnoalgL1XE10",&passnoalgL1XE10);
+    zeroBiasTree->SetBranchAddress("passnoalgL1XE30",&passnoalgL1XE30);
+    zeroBiasTree->SetBranchAddress("passnoalgL1XE40",&passnoalgL1XE40);
+    zeroBiasTree->SetBranchAddress("passnoalgL1XE45",&passnoalgL1XE45);
+
+
+    Int_t numbToKeep = 0 ;
+    for (Int_t i = 0 ; i < zerobiasNentries ; i++)
+    {
+        zeroBiasTree->GetEntry(i);
+        if ( metl1 > 50.0 && metcell > 100.0  &&
+           ( passnoalgL1XE10 > 0.5 || passnoalgL1XE30 > 0.5 ||
+            passnoalgL1XE40 > 0.5 || passnoalgL1XE45 > 0.5  ) )
+        {
+            numbToKeep++;
+        }
+    }
+
+    std::cout << "Number of events to keep: " << numbToKeep << std::endl;
+
+
+    return(numbToKeep);
+}
