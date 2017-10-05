b6a647af0b6251ef711ece8822b9ec907464b1af
Joseph Corrado <jgc375@nyu.edu>
2017-10-05 10:44:58 -0400

    Filled the userinfo struct in a separate file with the global information, so I can be sure that all the macros are using same set of globals. The other portion of the userfile info should be filled within threeefficiencies because they depend on the algorithms i use and what the htresholds are

---
+++ myMacros/globalParameterFile.C
@@ -0,0 +1,30 @@
+#include "userInfo.h"
+
+
+
+userInfo logFileParams;
+
+
+
+logFileParams.nbins = nbins;
+logFileParams.metmin = metMin;
+logFileParams.metmax = metMax;
+logFileParams.metl1thresh = metl1thresh;
+logFileParams.frac = frac;
+logFileParams.zbFileName = zerobiasFileName;
+logFileParams.muonFileName = muonFilename;
+logFileParams.muonNentries = muonNentries;
+logFileParams.zbNentries = zerobiasNentries;
+logFileParams.actintCut = actintCut;
+
+
+
+
+
+
+
+
+
+
+
+//TODO: define th evariables in the userinfo struct in this file. Needs to import userinfo.h
