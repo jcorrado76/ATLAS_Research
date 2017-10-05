2610c1682d2e3e71ce93071e1ad31f74a6f51030
Joseph Corrado <jgc375@nyu.edu>
2017-10-05 14:40:47 -0400

    Overriding the print operator for TObjects so I can make userinfo objects print themselves

--- myMacros/userInfo.h
+++ myMacros/userInfo.h
@@ -1,6 +1,6 @@
 #include "TObject.h"
 
-struct userInfo : public TObject {
+class userInfo : public TObject {
     TString algAName;
     TString algBName;
     Int_t nbins;
@@ -15,7 +15,6 @@ struct userInfo : public TObject {
     Float_t algBThresh;
     Int_t muonNentries;
     Int_t zbNentries;
-    Int_t numPassMuon;
     Int_t numMuonKeptCombined;
 
     Int_t numMuonPassNumeratorAlgA;
@@ -25,4 +24,27 @@ struct userInfo : public TObject {
     Float_t eps;
 
     Float_t actintCut;
+
+public:
+    void Print()
+    {
+        std::cout << "Alg A Name: " << algAName << std::endl;
+        std::cout << "Alg B Name: " << algBName << std::endl;
+        std::cout << "nbins: " << nbins << std::endl;
+        std::cout << "MetMin: " << metmin << std::endl;
+        std::cout << "MetMax: " << metmax << std::endl;
+        std::cout << "METL1 thresh used: " << metl1thresh << std::endl;
+        std::cout << "frac to keep: " << frac << std::endl;
+        std::cout << "Zero Bias File Used: " << zbFileName << std::endl;
+        std::cout << "Muon File Used: " << muonFilename << std::endl;
+        std::cout << "Number of zerobias events that passed no alg and metl1: " << numzbRndm << std::endl;
+        std::cout << "Threshold needed on A: " << algAThresh << std::endl;
+        std::cout << "Threshold needed on B: " << algBThresh << std::endl;
+        std::cout << "Muon Nentries: " << muonNentries << std::endl;
+        std::cout << "ZeroBias Nentries: " << zbNentries << std::endl;
+        std::cout << "Number of entries kept using combined thresholds: " <<  numMuonKeptCombined << std::endl;
+        std::cout << "Number of entries that passed all muon cuts and " + algAName + " at Individ thresh: " << numMuonPassNumeratorAlgA << std::endl;
+        std::cout << "Number of entries that passed all muon cuts and " + algBName + " at Individ thresh: " << numMuonPassNumeratorAlgB << std::endl;
+        std::cout << "Number of entries that passed all muon cuts and both algs at Combined thresh: " << numMuonPassNumeratorAlgC << std::endl;
+    }
 };
