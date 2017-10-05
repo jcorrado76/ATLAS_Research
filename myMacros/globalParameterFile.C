bf8173ac3e2e43d0585d7c1d2824ec0b91ab8262
Joseph Corrado <jgc375@nyu.edu>
2017-10-05 10:59:31 -0400

    Added the initialization of the parameters in the global parameters file

--- myMacros/globalParameterFile.C
+++ myMacros/globalParameterFile.C
@@ -1,9 +1,17 @@
 #include "userInfo.h"
 
 
-
+{
 userInfo logFileParams;
 
+Int_t nbins = 1200;
+Float_t metMin = 0.0;
+Float_t metMax = 300.0;
+Float_t metl1thresh = 50.0;
+Float_t frac = 0.0059;
+TString& zerobiasFileName = "PhysicsMain.All.noalgXEtriggers.2016.f731f758._m1659m1710.48Runs.root";
+TString& muonFilename = "PhysicsMain.L1KFmuontriggers.2016.f731f758_m1659m1710.Run309759.48Runs.root";
+Float_t actintCut = 35.0;
 
 
 logFileParams.nbins = nbins;
@@ -20,7 +28,7 @@ logFileParams.actintCut = actintCut;
 
 
 
-
+}
 
 
 
