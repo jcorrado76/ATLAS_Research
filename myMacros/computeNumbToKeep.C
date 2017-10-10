53698b9d8c8e51362b28a70ab59afba25a49237e
Joseph Corrado <jgc375@nyu.edu>
2017-10-10 12:54:31 -0400

    computing both the number of events kept with what CERN currently uses, and also computing number of events kept when I use actint instead of metcell

--- myMacros/computeNumbToKeep.C
+++ myMacros/computeNumbToKeep.C
@@ -43,8 +43,10 @@ Int_t computeNumbToKeep(const TString& zeroBiasFileName = "PhysicsMain.All.noalg
     this is what CERN currently uses. The fraction needs to stay the same; need to still
     use 1/200, but the number of events to keep changes based on the new actint cut, because you need to
     multiply fraction to keep (0.0059) by some new numbToKeep
+    */
 
-    Int_t numbToKeep = 0 ;
+    Int_t counter1 = 0 ;
+    Int_t counter2 = 0;
     for (Int_t i = 0 ; i < zerobiasNentries ; i++)
     {
         zeroBiasTree->GetEntry(i);
@@ -52,33 +54,33 @@ Int_t computeNumbToKeep(const TString& zeroBiasFileName = "PhysicsMain.All.noalg
            ( passnoalgL1XE10 > 0.5 || passnoalgL1XE30 > 0.5 ||
             passnoalgL1XE40 > 0.5 || passnoalgL1XE45 > 0.5  ) )
         {
-            numbToKeep++;
+            counter1++;
+        }
+        if ( (metl1 > 50.0) && (actint > actintCut)  &&
+           ( passnoalgL1XE10 > 0.5 || passnoalgL1XE30 > 0.5 ||
+            passnoalgL1XE40 > 0.5 || passnoalgL1XE45 > 0.5  ) )
+        {
+            counter2++;
         }
     }
-    */
+
 
 
     //we want to be able to look at how the threshold on cell needed to keep same fraction
     //varies as we change actint cut
     //this is not the numb to keep. this is the numb to keep, divided by the fraction to keep
 
+    Float_t counter1Frac = (Float_t) counter1 / (Float_t) zerobiasNentries;
+    Float_t counter2Frac = (Float_t) counter2 / (Float_t) zerobiasNentries;
 
-    //TODO: what should I call this number? it's not numb to keep, is it the denominator?
-    Int_t numbToKeep = 0 ;
-    for (Int_t i = 0 ; i < zerobiasNentries ; i++)
-    {
-        zeroBiasTree->GetEntry(i);
-        if ( (metl1 > 50.0) && (actint > actintCut)  &&
-           ( passnoalgL1XE10 > 0.5 || passnoalgL1XE30 > 0.5 ||
-            passnoalgL1XE40 > 0.5 || passnoalgL1XE45 > 0.5  ) )
-        {
-            numbToKeep++;
-        }
-    }
+    std::cout << "zb_Nentries: " << zerobiasNentries << std::endl;
+
+    std::cout << "Number of events kept with passnoalg, l1, and cell > 100:" << counter1 << std::endl;
+    std::cout << "Fraction of zb nentries: " << counter1Frac << std::endl;
+
+    std::cout << "Number of events kept with passsnoalg, l1, and actint > 35: " << counter2 << std::endl;
+    std::cout << "Fraction of zb nentries: " << counter2Frac << std::endl;
 
 
-    std::cout << "Number of events to keep: " << numbToKeep << std::endl;
-
-
-    return(numbToKeep);
+    return(1);
 }
