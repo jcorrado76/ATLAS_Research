f9cb62c76f48272eae29501fe0e4b102e19a1bcb
Joseph Corrado <jgc375@nyu.edu>
2017-10-11 16:38:07 -0400

    Changed names to be more descriptive. Wrote everything in terms of which process it belongs to.

--- myMacros/computeNumbToKeep.C
+++ myMacros/computeNumbToKeep.C
@@ -12,7 +12,7 @@
 #include "TSystem.h"
 #include "TF1.h"
 
-Int_t computeNumbToKeep(const TString& zeroBiasFileName = "PhysicsMain.All.noalgXEtriggers.2016.f731f758._m1659m1710.48Runs.root")
+Int_t computeNumbToKeep(const TString& thresholdFileName = "PhysicsMain.All.noalgXEtriggers.2016.f731f758._m1659m1710.48Runs.root")
 {
     /* given a new zerobias file, this macro computes what fraction the triggers currently in use at CERN would keep
     must pass metl1 > 50.0, cell > 100.0, and all  passnoalg_i > 0.5
@@ -20,23 +20,23 @@ Int_t computeNumbToKeep(const TString& zeroBiasFileName = "PhysicsMain.All.noalg
     */
 
 
-    TString zerobiasFilePath = "../myData/"+ zeroBiasFileName;
-    TFile * zeroBiasFile = TFile::Open(zerobiasFilePath, "READ");
-    TTree* zeroBiasTree = (TTree*)zeroBiasFile->Get("tree");
-    Int_t zerobiasNentries = zeroBiasTree->GetEntries();
+    TString threshFilePath = "../myData/"+ thresholdFileName;
+    TFile * threshFileHandle = TFile::Open(threshFilePath, "READ");
+    TTree* thresholdTree = (TTree*)threshFileHandle->Get("tree");
+    Int_t NentriesNoCuts = thresholdTree->GetEntries();
 
     Float_t metl1, metcell;
     Int_t passnoalgL1XE10 , passnoalgL1XE30, passnoalgL1XE40, passnoalgL1XE45;
     Float_t actint;
     Int_t actintCut = 35.0;
 
-    zeroBiasTree->SetBranchAddress("metl1",&metl1);
-    zeroBiasTree->SetBranchAddress("metcell", &metcell);
-    zeroBiasTree->SetBranchAddress("passnoalgL1XE10",&passnoalgL1XE10);
-    zeroBiasTree->SetBranchAddress("passnoalgL1XE30",&passnoalgL1XE30);
-    zeroBiasTree->SetBranchAddress("passnoalgL1XE40",&passnoalgL1XE40);
-    zeroBiasTree->SetBranchAddress("passnoalgL1XE45",&passnoalgL1XE45);
-    zeroBiasTree->SetBranchAddress("actint",&actint);
+    thresholdTree->SetBranchAddress("metl1",&metl1);
+    thresholdTree->SetBranchAddress("metcell", &metcell);
+    thresholdTree->SetBranchAddress("passnoalgL1XE10",&passnoalgL1XE10);
+    thresholdTree->SetBranchAddress("passnoalgL1XE30",&passnoalgL1XE30);
+    thresholdTree->SetBranchAddress("passnoalgL1XE40",&passnoalgL1XE40);
+    thresholdTree->SetBranchAddress("passnoalgL1XE45",&passnoalgL1XE45);
+    thresholdTree->SetBranchAddress("actint",&actint);
 
     /*
     To compute number of events that we know we can trigger on
@@ -46,21 +46,28 @@ Int_t computeNumbToKeep(const TString& zeroBiasFileName = "PhysicsMain.All.noalg
     */
 
     Int_t noActintProcess1EventsKept = 0 ;
-    Int_t counter2 = 0;
-    for (Int_t i = 0 ; i < zerobiasNentries ; i++)
+    Int_t noActintProcess2EventsKept = 0 ;
+    Int_t ActintCutProcess1EventsKept = 0;
+    for (Int_t i = 0 ; i < NentriesNoCuts ; i++)
     {
-        zeroBiasTree->GetEntry(i);
-        if ( metl1 > 50.0 && metcell > 100.0  &&
+        thresholdTree->GetEntry(i);
+        if ( metl1 > 50.0 &&
            ( passnoalgL1XE10 > 0.5 || passnoalgL1XE30 > 0.5 ||
             passnoalgL1XE40 > 0.5 || passnoalgL1XE45 > 0.5  ) )
         {
             noActintProcess1EventsKept++;
         }
+        if ( metl1 > 50.0 && metcell > 100.0  &&
+           ( passnoalgL1XE10 > 0.5 || passnoalgL1XE30 > 0.5 ||
+            passnoalgL1XE40 > 0.5 || passnoalgL1XE45 > 0.5  ) )
+        {
+            noActintProcess2EventsKept++;
+        }
         if ( (metl1 > 50.0) && (actint > actintCut)  &&
            ( passnoalgL1XE10 > 0.5 || passnoalgL1XE30 > 0.5 ||
             passnoalgL1XE40 > 0.5 || passnoalgL1XE45 > 0.5  ) )
         {
-            counter2++;
+            ActintCutProcess1EventsKept++;
         }
     }
 
@@ -70,16 +77,20 @@ Int_t computeNumbToKeep(const TString& zeroBiasFileName = "PhysicsMain.All.noalg
     //varies as we change actint cut
     //this is not the numb to keep. this is the numb to keep, divided by the fraction to keep
 
-    Float_t noActintProcess1Frac = (Float_t) noActintProcess1EventsKept / (Float_t) zerobiasNentries;
-    Float_t counter2Frac = (Float_t) counter2 / (Float_t) zerobiasNentries;
+    Float_t noActintProcess1Frac = (Float_t) noActintProcess1EventsKept / (Float_t) NentriesNoCuts;
+    Float_t noActintProcess2Frac = (Float_t) noActintProcess2EventsKept / (Float_t) noActintProcess1EventsKept;
+    Float_t ActintCutProcess1Frac = (Float_t) ActintCutProcess1EventsKept / (Float_t) NentriesNoCuts;
 
-    std::cout << "passnoalgNentries: " << zerobiasNentries << std::endl;
+    std::cout << "Entries kept with no cuts: " << NentriesNoCuts << std::endl;
 
-    std::cout << "Number of events kept with l1>50.0 and cell > 100.0: " << noActintProcess1EventsKept << std::endl;
+    std::cout << "Number of events kept with l1>50.0: " << noActintProcess1EventsKept << std::endl;
     std::cout << "Fraction of passnoalg: " << noActintProcess1Frac << std::endl;
 
-    std::cout << "Number of events kept with passsnoalg, l1, and actint > 35: " << counter2 << std::endl;
-    std::cout << "Fraction of zb nentries: " << counter2Frac << std::endl;
+    std::cout << "Number of events kept with l1>50.0 and cell > 100.0: " << noActintProcess2EventsKept << std::endl;
+    std::cout << "Fraction of noActint process 1: " << noActintProcess2Frac << std::endl;
+
+    std::cout << "Number of events kept with l1>50.0 and actint > 35: " << ActintCutProcess1EventsKept << std::endl;
+    std::cout << "Fraction of passnoalg: " << ActintCutProcess1Frac << std::endl;
 
 
     return(1);
