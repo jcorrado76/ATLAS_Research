4ba5e48fc40f9b3c96555f2cfdc8e3e997eb9e57
Joseph Corrado <jgc375@nyu.edu>
2017-10-28 16:22:31 -0400

    updated the compile file with the correct progress

--- HLTAnalysis/compile.C
+++ HLTAnalysis/compile.C
@@ -13,4 +13,24 @@ bool compile(const std::string& option = "")
     {
         gSystem->AddIncludePath("-Iinclude");
     }
+
+    //compile and load source
+
+    //compile each one; k means keep shared library after session end; call it "libHistTools", send to "lib"
+    if (gSystem->CompileMacro("source/threeEfficiencies.C", (option + "k-").c_str(), "libThreeEfficiencies", "lib") == 0) {return false;}
+    if (gSystem->CompileMacro("source/mincerMacros.C", (option + "k-").c_str(), "libMincerMacros", "lib") == 0) {return false;}
+    if (gSystem->CompileMacro("source/userInfo.C", (option + "k-").c_str(), "libUserInfo", "lib") == 0) {return false;}
+
+    //successfully compiled dependencies
+    return true;
+
+}
+
+//clean up library directory
+
+//usage: .L compile.C+; clean()
+
+void clean()
+{
+    gSystem->Exec("rm lib/*");
 }
