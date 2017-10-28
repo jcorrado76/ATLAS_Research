4757100cd4714c8ff721ffba447037c265d548b5
Joseph Corrado <jgc375@nyu.edu>
2017-10-28 16:24:32 -0400

    updated HLT project with proper progress

--- HLTAnalysis/source/bisection.C
+++ HLTAnalysis/source/bisection.C
@@ -1,19 +1,4 @@
-#include "TMath.h"
-#include "TH1.h"
-#include "TFile.h"
-#include "TTree.h"
-#include "TString.h"
-#include "TEfficiency.h"
-#include "TLegend.h"
-#include <iostream>
-#include <fstream>
-#include "TROOT.h"
-#include "TCanvas.h"
-#include "TSystem.h"
-#include "TF1.h"
-#include "TNtuple.h"
-#include "userInfo.h"
-
+#include "mincerMacros.h"
 
 
 //TODO: implement proof lite
