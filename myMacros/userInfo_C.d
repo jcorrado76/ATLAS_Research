db4e16cb7ffff91fc8e51d1e46dd024b4c3427a1
Joseph Corrado <jgc375@nyu.edu>
2017-11-12 21:59:09 -0500

    11.12 run

---
+++ myMacros/userInfo_C.d
@@ -0,0 +1,120 @@
+
+# DO NOT DELETE
+
+./userInfo_C.so: userInfo.h /usr/include/c++/5/string
+./userInfo_C.so: /usr/include/c++/5/iostream /usr/include/c++/5/fstream
+./userInfo_C.so: /home/joe/Documents/root/include/TMath.h
+./userInfo_C.so: /home/joe/Documents/root/include/Rtypes.h
+./userInfo_C.so: /home/joe/Documents/root/include/RtypesCore.h
+./userInfo_C.so: /home/joe/Documents/root/include/RConfig.h
+./userInfo_C.so: /home/joe/Documents/root/include/RVersion.h
+./userInfo_C.so: /home/joe/Documents/root/include/DllImport.h
+./userInfo_C.so: /home/joe/Documents/root/include/snprintf.h
+./userInfo_C.so: /home/joe/Documents/root/include/strlcpy.h
+./userInfo_C.so: /usr/include/c++/5/atomic /usr/include/c++/5/typeinfo
+./userInfo_C.so: /home/joe/Documents/root/include/TGenericClassInfo.h
+./userInfo_C.so: /usr/include/c++/5/vector
+./userInfo_C.so: /home/joe/Documents/root/include/TSchemaHelper.h
+./userInfo_C.so: /home/joe/Documents/root/include/TIsAProxy.h
+./userInfo_C.so: /home/joe/Documents/root/include/TVirtualIsAProxy.h
+./userInfo_C.so: /home/joe/Documents/root/include/TMathBase.h
+./userInfo_C.so: /usr/include/c++/5/cstdlib /usr/include/c++/5/cmath
+./userInfo_C.so: /home/joe/Documents/root/include/TError.h
+./userInfo_C.so: /usr/include/c++/5/algorithm /usr/include/c++/5/limits
+./userInfo_C.so: /home/joe/Documents/root/include/TH1.h
+./userInfo_C.so: /home/joe/Documents/root/include/TAxis.h
+./userInfo_C.so: /home/joe/Documents/root/include/TNamed.h
+./userInfo_C.so: /home/joe/Documents/root/include/TObject.h
+./userInfo_C.so: /home/joe/Documents/root/include/RConfigure.h
+./userInfo_C.so: /home/joe/Documents/root/include/TStorage.h
+./userInfo_C.so: /home/joe/Documents/root/include/TVersionCheck.h
+./userInfo_C.so: /usr/include/c++/5/iosfwd
+./userInfo_C.so: /home/joe/Documents/root/include/TString.h
+./userInfo_C.so: /home/joe/Documents/root/include/RStringView.h
+./userInfo_C.so: /home/joe/Documents/root/include/RWrap_libcpp_string_view.h
+./userInfo_C.so: /usr/include/c++/5/iterator /usr/include/c++/5/ostream
+./userInfo_C.so: /usr/include/c++/5/iomanip /usr/include/c++/5/stdexcept
+./userInfo_C.so: /home/joe/Documents/root/include/libcpp_string_view.h
+./userInfo_C.so: /home/joe/Documents/root/include/TAttAxis.h
+./userInfo_C.so: /home/joe/Documents/root/include/TArrayD.h
+./userInfo_C.so: /home/joe/Documents/root/include/TArray.h
+./userInfo_C.so: /home/joe/Documents/root/include/TAttLine.h
+./userInfo_C.so: /home/joe/Documents/root/include/TAttFill.h
+./userInfo_C.so: /home/joe/Documents/root/include/TAttMarker.h
+./userInfo_C.so: /home/joe/Documents/root/include/TArrayC.h
+./userInfo_C.so: /home/joe/Documents/root/include/TArrayS.h
+./userInfo_C.so: /home/joe/Documents/root/include/TArrayI.h
+./userInfo_C.so: /home/joe/Documents/root/include/TArrayF.h
+./userInfo_C.so: /home/joe/Documents/root/include/Foption.h
+./userInfo_C.so: /home/joe/Documents/root/include/TVectorFfwd.h
+./userInfo_C.so: /home/joe/Documents/root/include/TVectorDfwd.h
+./userInfo_C.so: /home/joe/Documents/root/include/TFitResultPtr.h
+./userInfo_C.so: /usr/include/c++/5/memory
+./userInfo_C.so: /home/joe/Documents/root/include/TFile.h
+./userInfo_C.so: /home/joe/Documents/root/include/TDirectoryFile.h
+./userInfo_C.so: /home/joe/Documents/root/include/TDirectory.h
+./userInfo_C.so: /home/joe/Documents/root/include/TBuffer.h
+./userInfo_C.so: /home/joe/Documents/root/include/TList.h
+./userInfo_C.so: /home/joe/Documents/root/include/TSeqCollection.h
+./userInfo_C.so: /home/joe/Documents/root/include/TCollection.h
+./userInfo_C.so: /home/joe/Documents/root/include/TIterator.h
+./userInfo_C.so: /home/joe/Documents/root/include/TDatime.h
+./userInfo_C.so: /home/joe/Documents/root/include/TUUID.h
+./userInfo_C.so: /home/joe/Documents/root/include/TMap.h
+./userInfo_C.so: /home/joe/Documents/root/include/THashTable.h
+./userInfo_C.so: /home/joe/Documents/root/include/TUrl.h
+./userInfo_C.so: /home/joe/Documents/root/include/ROOT/TRWSpinLock.hxx
+./userInfo_C.so: /home/joe/Documents/root/include/ROOT/TSpinMutex.hxx
+./userInfo_C.so: /usr/include/c++/5/condition_variable
+./userInfo_C.so: /usr/include/c++/5/mutex
+./userInfo_C.so: /home/joe/Documents/root/include/TTree.h
+./userInfo_C.so: /home/joe/Documents/root/include/TBranch.h
+./userInfo_C.so: /home/joe/Documents/root/include/TObjArray.h
+./userInfo_C.so: /home/joe/Documents/root/include/TDataType.h
+./userInfo_C.so: /home/joe/Documents/root/include/TDictionary.h
+./userInfo_C.so: /home/joe/Documents/root/include/ESTLType.h
+./userInfo_C.so: /home/joe/Documents/root/include/TClass.h
+./userInfo_C.so: /home/joe/Documents/root/include/TObjString.h
+./userInfo_C.so: /usr/include/c++/5/map /usr/include/c++/5/set
+./userInfo_C.so: /usr/include/c++/5/unordered_set
+./userInfo_C.so: /home/joe/Documents/root/include/ThreadLocalStorage.h
+./userInfo_C.so: /home/joe/Documents/root/include/TVirtualTreePlayer.h
+./userInfo_C.so: /home/joe/Documents/root/include/TROOT.h
+./userInfo_C.so: /home/joe/Documents/root/include/TEfficiency.h
+./userInfo_C.so: /usr/include/c++/5/utility
+./userInfo_C.so: /home/joe/Documents/root/include/TLegend.h
+./userInfo_C.so: /home/joe/Documents/root/include/TPave.h
+./userInfo_C.so: /home/joe/Documents/root/include/TBox.h
+./userInfo_C.so: /home/joe/Documents/root/include/TAttBBox2D.h
+./userInfo_C.so: /home/joe/Documents/root/include/GuiTypes.h
+./userInfo_C.so: /home/joe/Documents/root/include/TPoint.h
+./userInfo_C.so: /home/joe/Documents/root/include/TAttText.h
+./userInfo_C.so: /home/joe/Documents/root/include/TCanvas.h
+./userInfo_C.so: /home/joe/Documents/root/include/TPad.h
+./userInfo_C.so: /home/joe/Documents/root/include/TVirtualPad.h
+./userInfo_C.so: /home/joe/Documents/root/include/TAttPad.h
+./userInfo_C.so: /home/joe/Documents/root/include/TVirtualX.h
+./userInfo_C.so: /home/joe/Documents/root/include/Buttons.h
+./userInfo_C.so: /home/joe/Documents/root/include/TQObject.h
+./userInfo_C.so: /home/joe/Documents/root/include/TVirtualQConnection.h
+./userInfo_C.so: /home/joe/Documents/root/include/TInterpreter.h
+./userInfo_C.so: /home/joe/Documents/root/include/TVirtualMutex.h
+./userInfo_C.so: /home/joe/Documents/root/include/TAttCanvas.h
+./userInfo_C.so: /home/joe/Documents/root/include/TCanvasImp.h
+./userInfo_C.so: /home/joe/Documents/root/include/TSystem.h
+./userInfo_C.so: /home/joe/Documents/root/include/TInetAddress.h
+./userInfo_C.so: /home/joe/Documents/root/include/TTimer.h
+./userInfo_C.so: /home/joe/Documents/root/include/TSysEvtHandler.h
+./userInfo_C.so: /home/joe/Documents/root/include/TTime.h
+./userInfo_C.so: /home/joe/Documents/root/include/TF1.h
+./userInfo_C.so: /usr/include/c++/5/functional
+./userInfo_C.so: /home/joe/Documents/root/include/TFormula.h
+./userInfo_C.so: /home/joe/Documents/root/include/TMethodCall.h
+./userInfo_C.so: /usr/include/c++/5/list
+./userInfo_C.so: /home/joe/Documents/root/include/Math/ParamFunctor.h
+./userInfo_C.so: /home/joe/Documents/root/include/TNtuple.h
+./userInfo_C.so: /home/joe/Documents/root/include/TBenchmark.h
+./userInfo_C.so: /home/joe/Documents/root/include/TStopwatch.h
+./userInfo_C.so: /home/joe/Documents/root/include/TChain.h
+./userInfo_C.so: /home/joe/Documents/root/include/RVersion.h /home/joe/Documents/root/include/RConfig.h /home/joe/Documents/root/include/TClass.h /home/joe/Documents/root/include/TDictAttributeMap.h /home/joe/Documents/root/include/TInterpreter.h /home/joe/Documents/root/include/TROOT.h /home/joe/Documents/root/include/TBuffer.h /home/joe/Documents/root/include/TMemberInspector.h /home/joe/Documents/root/include/TError.h /home/joe/Documents/root/include/RtypesImp.h /home/joe/Documents/root/include/TIsAProxy.h /home/joe/Documents/root/include/TFileMergeInfo.h /home/joe/Documents/root/include/TCollectionProxyInfo.h /home/joe/Documents/root/bin/rootcling
+userInfo_C__ROOTBUILDVERSION= 6.10/00
