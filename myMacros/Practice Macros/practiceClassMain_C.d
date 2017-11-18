a99603ee7b8e2790c1d5c5094a477937e4089eb3
Joseph Corrado <jgc375@nyu.edu>
2017-11-18 13:54:54 -0500

    removed tracked files from the git index

--- myMacros/Practice Macros/practiceClassMain_C.d
+++
@@ -1,4 +0,0 @@
-
-# DO NOT DELETE
-./practiceClassMain_C.so: /home/joe/Documents/root/include/RVersion.h /home/joe/Documents/root/include/RConfig.h /home/joe/Documents/root/include/TClass.h /home/joe/Documents/root/include/TDictAttributeMap.h /home/joe/Documents/root/include/TInterpreter.h /home/joe/Documents/root/include/TROOT.h /home/joe/Documents/root/include/TBuffer.h /home/joe/Documents/root/include/TMemberInspector.h /home/joe/Documents/root/include/TError.h /home/joe/Documents/root/include/RtypesImp.h /home/joe/Documents/root/include/TIsAProxy.h /home/joe/Documents/root/include/TFileMergeInfo.h /home/joe/Documents/root/include/TCollectionProxyInfo.h /home/joe/Documents/root/bin/rootcling
-practiceClassMain_C__ROOTBUILDVERSION= 6.10/00
--- myMacros/Practice Macros/test.C
+++
@@ -1,9 +0,0 @@
-
-Int_t test()
-{
-gROOT->ProcessLine(".L generateEfficiencies.C");
-gROOT->ProcessLine(".L print7Efficiencies.C");
-gROOT->ProcessLine(".x generateEfficiencies.C()");
-gROOT->ProcessLine(".x print7Efficiencies.C()");
-return(0);
-}
