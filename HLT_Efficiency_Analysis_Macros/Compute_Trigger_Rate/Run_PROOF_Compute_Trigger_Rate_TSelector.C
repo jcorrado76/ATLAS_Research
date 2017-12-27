1989a693eecfceb32ccf28dc3f9484c07bb22cd5
Joseph Corrado <josephcorrado7@gmail.com>
2017-12-26 22:32:55 -0500

    Successfully Parallellized macro that computes trigger rate to keep given a ZB file.

---
+++ HLT_Efficiency_Analysis_Macros/Compute_Trigger_Rate/Run_PROOF_Compute_Trigger_Rate_TSelector.C
@@ -0,0 +1,18 @@
+
+
+
+//usage of the selector without PROOF: 
+//TFile* myfile = TFile::Open("../../../MyData/ZeroBiasKF....");
+//tree->Process("Compute_Trigger_Rate_TSelector.C+");
+
+//usage of this PROOF macro:
+//.x Run_PROOF_Compute_Trigger_Rate_TSelector.C 
+
+{
+    TProof::Open("");
+    TChain* mychain = new TChain( "tree" , "Compute_Trigger_Rate_Chain" );
+    mychain->Add("../../../MyData/ZeroBiasL1KF2016R307195R311481.51Runs.root");
+    mychain->SetProof();
+    mychain->Process("Compute_Trigger_Rate_TSelector.C+");
+}
+
