f9cfc6e7550d34bfbe0cc2d9a033a2dc5a98c8cf
Joseph <josephcorrado7@gmail.com>
2018-01-12 17:32:42 -0500

    Added description

--- HLT_Efficiency_Analysis_Macros/rootlogon.C
+++ HLT_Efficiency_Analysis_Macros/rootlogon.C
@@ -1,6 +1,7 @@
 //The macro, rootlogon.C is executed when root starts, unless -n is passed
 //
-
+//this macro will start up the ATLAS research environment, including my helper
+//macros, and my custom class that records the parameters of bisection
 
 {
     std::cout << "Started ATLAS_Research Environmnet" << std::endl;
