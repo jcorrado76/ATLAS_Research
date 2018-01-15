e83782a0898f97e26298f9eda93fbf2994b9c34e
Joseph <josephcorrado7@gmail.com>
2018-01-15 18:30:30 -0500

    Trying to use ghost printing in order to figure out what's wrong with the data file path variable

--- HLT_Efficiency_Analysis_Macros/Run3Eff/Perform_Efficiency_Analysis.C
+++ HLT_Efficiency_Analysis_Macros/Run3Eff/Perform_Efficiency_Analysis.C
@@ -4,18 +4,20 @@
 TFile* threeEfficiencies( const TString& AlgAName , const TString& AlgBName )
 {
 
-    const TString DATA_PATH( "../../../ATLAS_DATA/" );
+    TString DATA_PATH =  "../../ATLAS_DATA/" ;
+    std::cout << "Data Path: " << DATA_PATH << std::endl;
 
     //GLOBAL DEFINITION
     userInfo* parameters = new userInfo();
     parameters->Set_AlgAName(AlgAName);
     parameters->Set_AlgBName(AlgBName);
-
+    //read the parameter file for HLT analysis
     parameters->Read_Parameter_File("./parameters.txt");
 
     //FILES
     TString zerobiasFileName = parameters->Get_ThreshFileName();
-    TString muonFilename = parameters->Get_MuonFileName();
+    const TString muonFilename = parameters->Get_MuonFileName();
+    std::cout << "Muon filename: " << muonFilename << std::endl;
 
     //THREEFF BENCHMARK
     TBenchmark* threeEfficienciesBenchmark = new TBenchmark();
@@ -24,7 +26,8 @@ TFile* threeEfficiencies( const TString& AlgAName , const TString& AlgBName )
     threeEfficienciesBenchmark->Start("Three Efficiencies");
 
     //MUON FILE; MUON TREE
-    TString muonFilePath = DATA_PATH + muonFilename;
+    const TString muonFilePath = DATA_PATH + muonFilename;
+    std::cout << "MuonFilePath: " << muonFilePath << std::endl;
     TFile * muonFile = TFile::Open(muonFilePath, "READ");
     TTree* myMuonTree = (TTree*)muonFile->Get("tree");
     Int_t muonNentries = myMuonTree->GetEntries();
