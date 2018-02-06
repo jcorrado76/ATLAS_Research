36ad3279d4463ea661fa10ee0497f6a6370fe41e
Joseph <josephcorrado7@gmail.com>
2018-02-06 16:13:29 -0500

    Re-added the efficiency and the histogram plots. Changed cut for efficiency to be XE>20

--- Curve_Reconstruction/ZB_Eff_Selector.C
+++ Curve_Reconstruction/ZB_Eff_Selector.C
@@ -10,14 +10,22 @@ void ZB_Eff_Selector::Begin(TTree * /*tree*/)
 void ZB_Eff_Selector::SlaveBegin(TTree * /*tree*/)
 {
    TString option = GetOption();
-   histo = new TH1F( "histo", "Unbiased Curves" , 400 , 0.0 , 300.0 );
-   fOutput->Add( histo );
+   histo1 = new TH1F( "histo1", "Unbiased Curves" , 400 , 0.0 , 300.0 );
+   histo2 = new TH1F( "histo2", "Unbiased Curves" , 400 , 0.0 , 300.0 );
+   XE_Ratio = new TEfficiency( "teff" , "Efficiency of ZB to ZB XE 20" , 400 , 0.0 , 300.0 );
+   fOutput->Add( histo1 );
+   fOutput->Add( histo2 );
+   fOutput->Add( XE_Ratio );
 }
 
 Bool_t ZB_Eff_Selector::Process(Long64_t entry)
 {
    fReader.SetEntry(entry);
-   histo->Fill( *metcell );
+   if (*passrndm > 0.0 ){
+       histo1->Fill( *metcell );
+       histo2->Fill( *metcell );
+       XE_Ratio->Fill( *metl1 > 20.0 , *metcell );
+   }
    return kTRUE;
 }
 
@@ -30,9 +38,16 @@ void ZB_Eff_Selector::Terminate()
     TCanvas* Histo_Canv = new TCanvas( "mycanv" , "Random Trigger Curves" );
     TString alg_name = "metcell";
     Histo_Canv->RangeAxis( 0.0 , 0.0 , 500.0 , 1.0 );
-    histo->Draw();
+    histo1->SetLineColor(kBlue);
+    histo2->SetLineColor(kTeal);
+    XE_Ratio->SetLineColor(kRed);
+    histo1->Draw();
+    histo2->Draw();
+    XE_Ratio->Draw();
     TLegend *legend = new TLegend( 0.57, 0.12 , 0.9, 0.4 , "" , "NDC");
-    legend->AddEntry( histo , alg_name );
+    legend->AddEntry( histo1 , "metcell plot");
+    legend->AddEntry( histo2 , "metcell plot");
+    legend->AddEntry( XE_Ratio , "Efficiency of ZB to ZBXE20");
     legend->Draw();
     Histo_Canv->Draw();
 }
