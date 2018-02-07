d2866980a28b7b062b6b28a658cca0218cd3071a
Joseph <josephcorrado7@gmail.com>
2018-02-07 17:47:30 -0500

    Changed my axes, and got both of the histograms to plot on the same canvas

--- Curve_Reconstruction/ZB_Eff_Selector.C
+++ Curve_Reconstruction/ZB_Eff_Selector.C
@@ -10,9 +10,9 @@ void ZB_Eff_Selector::Begin(TTree * /*tree*/)
 void ZB_Eff_Selector::SlaveBegin(TTree * /*tree*/)
 {
    TString option = GetOption();
-   histo1 = new TH1F( "histo1", "Random Trigger" , 400 , 0.0 , 300.0 );
-   histo2 = new TH1F( "histo2", "Random Trigger XE20" , 400 , 0.0 , 300.0 );
-   XE_Ratio = new TEfficiency( "teff" , "Efficiency" , 400 , 0.0 , 300.0 );
+   histo1 = new TH1F( "histo1", "Random Trigger" , 400 , 0.0 , 100.0 );
+   histo2 = new TH1F( "histo2", "Random Trigger XE20" , 400 , 0.0 , 100.0 );
+   XE_Ratio = new TEfficiency( "teff" , "Efficiency" , 400 , 0.0 , 100.0 );
    fOutput->Add( histo1 );
    fOutput->Add( histo2 );
    fOutput->Add( XE_Ratio );
@@ -40,12 +40,14 @@ void ZB_Eff_Selector::Terminate()
     TCanvas* random_canv = new TCanvas( "mycanv" , "Random Trigger Curves" );
     gStyle->SetOptStat(kFALSE);
     TString alg_name = "metcell";
-    random_canv->RangeAxis( 0.0 , 0.0 , 500.0 , 1.0 );
+    random_canv->RangeAxis( 1.0 , 1.0 , 500.0 , 1.0 );
     histo1->SetLineColor(kBlue);
-    histo2->SetLineColor(kPurple);
+    histo2->SetLineColor(kMagenta);
+    histo2->SetAxisRange(1.0,5.0e5,"Y");
     XE_Ratio->SetLineColor(kRed);
+    random_canv->SetLogy();
     histo1->Draw();
-    histo2->Draw();
+    histo2->Draw("SAME");
     //XE_Ratio->Draw();
     TLegend *legend = new TLegend( 0.57, 0.12 , 0.9, 0.4 , "" , "NDC");
     legend->AddEntry( histo1 , "Metcell Random");
