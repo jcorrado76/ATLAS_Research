8be9abb6eb5eea157ec97617dbe37e49a91f649c
Joseph <josephcorrado7@gmail.com>
2018-02-22 02:06:03 -0500

    The fitting works, but something is wrong with the function / parameters. I SHould speak to Allen about my derivation and make sure everything was done correctly

--- Curve_Reconstruction/efficiency_fitter.cpp
+++ Curve_Reconstruction/efficiency_fitter.cpp
@@ -1,13 +1,11 @@
 
-    Float_t PI = TMath::Pi();
-    Double_t fit( Float_t *x , Float_t* par )
+    Double_t fit( Double_t *x , Double_t *par )
     {
-        par[0] = 1.0;
-        par[1] = 0.0;
-        par[2] = 1.0;
+        Double_t PI = TMath::Pi();
+        Double_t arg = 0;
 
         Float_t l1cut = 20.0;
-        Double_t arg = ( l1cut  - par[0]*x[0]+ par[1])/(par[2]*TMath::Sqrt(2.0));
+        arg = ( l1cut  - par[0]*x[0]+ par[1])/(par[2]*TMath::Sqrt(2.0));
         Double_t fitval = (1./(2*par[2]*TMath::Sqrt(2*PI)))-(1/TMath::Sqrt(PI))*TMath::Erf(arg);
 
         return(fitval );
@@ -23,7 +21,7 @@ void efficiency_fitter(){
     //I set the range of the Tf1 to be up to 60 GeV because beyond that the error bars are so large those points don't
     //work to pull the fit at all
 
-    TF1* erf = new TF1( "erf" , fit , 0.0 , 100.0 , 3);
+    TF1* erf = new TF1( "fit" , fit , 0.0 , 100.0 , 3);
 
     //set the normalization to 1
     erf->SetParameter(0, 1.);
@@ -32,6 +30,7 @@ void efficiency_fitter(){
     //set sigma to 1
     erf->SetParameter(2, 1.);
 
+    erf->SetParNames("Slope","Translation","Sigma");
     //"R" tells the fit function from BinomialEfficiency::Fit to use the range of the TF1 as the fitting range
     eff->Fit( erf, "R" );
 
