bbed6ef92308a5173b23ef562727116b3adf8046
Joseph <josephcorrado7@gmail.com>
2018-02-22 16:55:44 -0500

    Got rid of some extraneous vestigial variables from previous fit

--- Curve_Reconstruction/efficiency_fitter.cpp
+++ Curve_Reconstruction/efficiency_fitter.cpp
@@ -1,25 +1,15 @@
-
     Double_t fit( Double_t *x , Double_t *par )
     {
-        Double_t PI = TMath::Pi();
-        Double_t arg = 0;
-
         Float_t l1cut = 20.0;
-
         Double_t fitval = (1./2.)*(1.+TMath::Erf((par[0]*x[0]+par[1]-l1cut)/(par[2]*TMath::Sqrt(2.))));
         return(fitval);
     }
 
 
 void efficiency_fitter(){
-
     TFile* myfile = TFile::Open("temp.root","READ");
-
     TEfficiency* eff = (TEfficiency*) myfile->Get("teff");
-
-    //TF1* erf = new TF1( "fit" , fit , 0.0 , 105.0 , 3);
     TF1* erf = new TF1( "fit" , fit , 0.0 , 200.0 , 3);
-
     //set the normalization to 1
     erf->SetParameter(0, 1.);
     //set the x translation to 0
