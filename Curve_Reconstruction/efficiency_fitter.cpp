43eb059d937f5d651f49200d326d10e1f7a7a037
Joseph <josephcorrado7@gmail.com>
2018-02-22 16:54:57 -0500

    Added comments about initializing parameters well, and got rid of previous derivation of fit function

--- Curve_Reconstruction/efficiency_fitter.cpp
+++ Curve_Reconstruction/efficiency_fitter.cpp
@@ -5,8 +5,6 @@
         Double_t arg = 0;
 
         Float_t l1cut = 20.0;
-        //arg = ( l1cut  - par[0]*x[0]+ par[1])/(par[2]*TMath::Sqrt(2.0));
-        //Double_t fitval = (1./(2*par[2]*TMath::Sqrt(2*PI)))-(1/TMath::Sqrt(PI))*TMath::Erf(arg);
 
         Double_t fitval = (1./2.)*(1.+TMath::Erf((par[0]*x[0]+par[1]-l1cut)/(par[2]*TMath::Sqrt(2.))));
         return(fitval);
@@ -19,9 +17,6 @@ void efficiency_fitter(){
 
     TEfficiency* eff = (TEfficiency*) myfile->Get("teff");
 
-    //I set the range of the Tf1 to be up to 60 GeV because beyond that the error bars are so large those points don't
-    //work to pull the fit at all
-
     //TF1* erf = new TF1( "fit" , fit , 0.0 , 105.0 , 3);
     TF1* erf = new TF1( "fit" , fit , 0.0 , 200.0 , 3);
 
@@ -29,8 +24,9 @@ void efficiency_fitter(){
     erf->SetParameter(0, 1.);
     //set the x translation to 0
     erf->SetParameter(1, 0.);
-    //set sigma to 1
+    //initialize sigma to 10
     erf->SetParameter(2, 10.);
+    //initializing parameters reasonably is important because it is a maximum likelihood fit
 
     erf->SetParNames("Slope","Translation","Sigma");
     //"R" tells the fit function from BinomialEfficiency::Fit to use the range of the TF1 as the fitting range
