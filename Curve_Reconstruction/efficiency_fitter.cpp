2b2ed841789d3be226f6334431e28c2722e3d512
Joseph <josephcorrado7@gmail.com>
2018-02-22 01:57:38 -0500

    Set parameters to initial values, implemented fitting. ready for testing

--- Curve_Reconstruction/efficiency_fitter.cpp
+++ Curve_Reconstruction/efficiency_fitter.cpp
@@ -8,20 +8,13 @@
 
         Float_t l1cut = 20.0;
         Double_t arg = ( l1cut  - par[0]*x[0]+ par[1])/(par[2]*TMath::Sqrt(2.0));
-        Double_t fitval = (1./(2*par[2]*TMath::Sqrt(2*TMath::Pi())))-(1/TMath::Sqrt(TMath::Pi()))*TMath::Erf(arg);
+        Double_t fitval = (1./(2*par[2]*TMath::Sqrt(2*PI)))-(1/TMath::Sqrt(PI))*TMath::Erf(arg);
 
         return(fitval );
     }
 
 
 void efficiency_fitter(){
-
-
-
-    Float_t hltcut = 0.0;
-
-
-    std::cout << fit(5.,1.,1.,1.,1.) << std::endl;
 
     TFile* myfile = TFile::Open("temp.root","READ");
 
@@ -30,19 +23,14 @@ void efficiency_fitter(){
     //I set the range of the Tf1 to be up to 60 GeV because beyond that the error bars are so large those points don't
     //work to pull the fit at all
 
-
-
-
-
-
-
-    TF1* erf = new TF1( "erf" , fit , 0.0 , 100.0 , 4);
+    TF1* erf = new TF1( "erf" , fit , 0.0 , 100.0 , 3);
 
     //set the normalization to 1
     erf->SetParameter(0, 1.);
     //set the x translation to 0
     erf->SetParameter(1, 0.);
-    //division by 1 
+    //set sigma to 1
+    erf->SetParameter(2, 1.);
 
     //"R" tells the fit function from BinomialEfficiency::Fit to use the range of the TF1 as the fitting range
     eff->Fit( erf, "R" );
