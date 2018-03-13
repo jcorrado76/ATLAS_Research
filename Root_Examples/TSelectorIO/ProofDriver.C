0249fc0e9d05110c53c59de35c62f01f2ce63197
Joseph <josephcorrado7@gmail.com>
2018-03-13 16:32:41 -0400

    Started writing example of how to use TSelectors for IO

---
+++ Root_Examples/TSelectorIO/ProofDriver.C
@@ -0,0 +1,20 @@
+{
+
+    TFile* ZeroBiasFile = TFile::Open("$DATA/ZeroBiasL1KF2016R307195R311481.51Runs.root");
+
+    if (ZeroBiasFile->IsOpen()){
+        std::cout << "File successfully opened" << std::endl;
+
+    }
+    else{
+        std::cout << "File not successfully opened. Terminating." std::endl;
+
+
+
+
+
+
+
+
+}
+
---
+++ Root_Examples/TSelectorIO/TSelectorIO.C
@@ -0,0 +1,36 @@
+#define TSelectorIO_cxx
+
+
+#include "TSelectorIO.h"
+#include <TH2.h>
+#include <TStyle.h>
+
+void TSelectorIO::Begin(TTree * /*tree*/)
+{
+
+   TString option = GetOption();
+}
+
+void TSelectorIO::SlaveBegin(TTree * /*tree*/)
+{
+
+   TString option = GetOption();
+
+}
+
+Bool_t TSelectorIO::Process(Long64_t entry)
+{
+
+   fReader.SetEntry(entry);
+
+   return kTRUE;
+}
+
+void TSelectorIO::SlaveTerminate()
+{
+}
+
+void TSelectorIO::Terminate()
+{
+
+}
