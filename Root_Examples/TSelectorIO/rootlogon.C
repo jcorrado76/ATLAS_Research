f8922698cea39f038c670a4baa47dc380bb0a5f3
Joseph <josephcorrado7@gmail.com>
2018-03-13 17:48:39 -0400

    Moving files around and changing settings until modular setup works

--- Root_Examples/TSelectorIO/rootlogon.C
+++ Root_Examples/TSelectorIO/rootlogon.C
@@ -1,18 +1,12 @@
-
-//ROOTLOGON.C
-
 {
-
     std::cout << "Loaded TSelectorIO Env" << std::endl;
-
-    gROOT->ProcessLine(".include /inclue");
-    std::cout << "Added include directory to include path" << std::endl;
+    //shouldn't need this line because i've included it in rootrc
+    //gROOT->ProcessLine(".include ./include");
 
     //gROOT->ProcessLine(".L /myLib/sharedlib.so");
-    std::cout << "Loaded Shared libraries" << std::endl;
-
-    /* Can add directory to ACLIC by using:
-     * gSystem->AddIncludePath(" -I/my/include/dir ");
-     * gSystem->AddLinkedLibs(" /mu/lib/libshared.so");*/
+    
+    //shouldn't need this line because i've included it in rootrc
+    //gSystem->AddIncludePath(" -I/include ");
+    gSystem->AddLinkedLibs("-L/src/TSelector_C.so");
 }
     
