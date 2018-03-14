d34acdf6a0fae9e1befa9883f9c2f965d6a44bf2
Joseph <josephcorrado7@gmail.com>
2018-03-14 15:48:11 -0400

    refactoring code into smaller macros

--- Jburr_Dataset_Analysis/src/EventsWithinRuns.C
+++ Jburr_Dataset_Analysis/src/EventsWithinRuns.C
@@ -1,16 +1,19 @@
 #include <TH1F.h>
 #include <TFile.h>
 #include <TNtuple.h>
-
-
+#include <TList.h>
+#include <TString.h>
 
 
 
 void EventsWithinRuns(){
+    /* This macro takes in a histogram containing the difference between two other histograms, and displays the bin
+     * number, number of entries in that bin, and the run number in which there is a nonzero difference */
 
 
     TFile* diffhistfile = TFile::Open("$jburr/RunNumberHists.root");
 
+    //histo3 is the difference between jburr and mincer hists
     TH1F* diffhist = (TH1F*)diffhistfile->Get("histo3");
 
     Int_t Nentries = 0;
@@ -35,6 +38,15 @@ void EventsWithinRuns(){
 
     nonzeroRuns->Scan();
   
+    TList* histlist = 0;
+
+    TString histname = "hist";
+    TH1F* histTemp = 0;
+    for ( Int j = 0 ; j < nonzeroRuns->GetEntries() ; j++ ){
+        histname += j;
+        histTemp = new TH1F(histname,histname,5000);
+
+
 
 
 
