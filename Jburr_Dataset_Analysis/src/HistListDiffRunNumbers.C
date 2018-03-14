8cd43350436512c616616448594ea936bfca1702
Joseph <josephcorrado7@gmail.com>
2018-03-14 16:08:22 -0400

    Created the lists and started working on the loop to fill the appropriate histograms / tcanvases

--- Jburr_Dataset_Analysis/src/HistListDiffRunNumbers.C
+++ Jburr_Dataset_Analysis/src/HistListDiffRunNumbers.C
@@ -11,12 +11,78 @@ void HistListDiffRunNumbers(){
     //jburr's events and see what the overlap by event number is. This would show if either of them included the same
     //file twice, and in which events within a run there is a difference
     
-    TList* histlist = 0;
+    TFile* mincerfile = TFile::Open("$DATA/ZeroBiasL1KF2016R307195R311481.51Runs.root","READ");  
+    TFile* jburrfile = TFile::Open("data/totalntuple16.root","READ");
 
-    TString histname = "hist";
-    TH1F* histTemp = 0;
+    TTree* mincertree = (TTree*) mincerfile->Get("tree");
+    TTree* burrtree = (TTree*) jburrfile->Get("METTree");
+
+    UInt_t RunNumber;
+    //TODO: needs to be changed to maximum and minimum event number...
+    Int_t runLow = 307195;
+    Int_t runHigh = 311481;
+    Int_t nbins = 10000;
+
+    Int_t mincerRunNumber, jburrRunNumber;
+    Int_t mincerpassrndm;
+    Bool_t jburrpassrndm;
+
+    TList* mincerhistlist = 0;
+    TList* staggeredmincerhistlist = 0;
+    TList* burrhistlist = 0;
+    TList* canvaslist = 0;
+
+    TH1F* mincerhist = 0;    
+    TH1F* burrhist = 0;  
+
+    TString* mincerhistname = "mincerhist";
+    TString* burrhistname = "burrhist"
+   
     for ( Int j = 0 ; j < nonzeroRuns->GetEntries() ; j++ ){
-        histname += j;
-        histTemp = new TH1F(histname,histname,5000);
+        //for each nonzero run number, fill 2 histograms with events distributed by event number 
+        //append each hist to the corresponding tlist
+        //then make the difference hist ; add to corresponding tlist
+        //then make the canvas of the staggered version of the hist and add to corresponding tlist 
+
+        mincerhistname += j;
+        burrhistname += j;
+
+        mincerhist = new TH1F( mincerhistname , mincerhistname , nbins , runLow , runHigh );
+        burrhist = new TH1F( burrhistname , burrhistname , nbins , runLow , runHigh);
+
 
     }
+
+
+
+
+    mincertree->SetBranchAddress("runnum",&mincerRunNumber);
+    mincertree->SetBranchAddress("passrndm",&mincerpassrndm);
+    burrtree->SetBranchAddress("HLT_noalg_zb_L1ZB.passed",&jburrpassrndm);
+    burrtree->SetBranchAddress("RunNumber",&RunNumber);
+
+
+    Int_t mincerentries = mincertree->GetEntries();
+    Int_t burrentries = burrtree->GetEntries();
+
+
+    for ( Int_t i = 0 ; i < mincerentries ; i++){
+        mincertree->GetEntry(i);
+        if (mincerpassrndm > 0.5 && mincerRunNumber < 310000 ){
+        mincerhist->Fill(mincerRunNumber + 10.);
+        }
+    }
+
+    for ( Int_t i = 0 ; i < burrentries; i++){
+        burrtree->GetEntry(i);
+        if (jburrpassrndm && RunNumber < runHigh && RunNumber > runLow){
+        burrhist->Fill(RunNumber);
+        }
+    }
+
+    mincerhist->SetLineColor(kRed);
+    burrhist->SetLineColor(kBlue);
+
+    //create a new file containing the Tlists of jburr hists, mincer hists, diff hists, and combined tcanvases
+    TFile* outfile = new TFile::Open("HistList.root", "RECREATE");
+}
