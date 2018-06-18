49279d7bd129bd58976977c33e96d05d99896e4e
Joseph <josephcorrado7@gmail.com>
2018-06-18 13:52:24 -0400

    Moved body of loop to new function

--- Mu_Analysis/fitEfficiencies/fit_efficiencies.C
+++ Mu_Analysis/fitEfficiencies/fit_efficiencies.C
@@ -10,21 +10,9 @@ Double_t fit( Double_t *x , Double_t *par )
     return(fitval);
 }
 
-
-
-void fit_efficiencies(){
-    // suppose there exists a root file containing a TClonesArray of Efficiency Curves
-    TFile* efficiencyCurveFile = TFile::Open("efficiencyCurves.root");
-    // how many efficiency curves to fit
-    Int_t numberSlices = 7;
-    // initialize TClonesArray to hold the efficiency curves
-    TClonesArray* efficiencyArray = new TClonesArray( "TEfficiency", numberSlices );
-    // initialize TClonesArray to hold the fit objects 
-    TClonesArray* efficiencyFitArray = new TClonesArray( "TF1", numberSlices );
-
-    // initialize the TEfficiency object that will hold each TEfficiency on each iteration 
-    TEfficiency* efficiencyObject = 0;
-    for ( int i = 0 ; i < numberSlices ; i++){
+TF1* generateFitFunction(){
+        // initialize the TEfficiency object that will hold each TEfficiency on each iteration 
+        TEfficiency* efficiencyObject = 0;
         TF1* fitErrorFunction = new TF1( "fit" , fit , 0.0 , 105.0 , 3);
 
         //set the normalization to 1
@@ -47,6 +35,22 @@ void fit_efficiencies(){
         std::cout << "Value of error on b: " << fitErrorFunction->GetParError(1) << std::endl;
         std::cout << "Value of fit for sigma: " << fitErrorFunction->GetParameter(2) << std::endl;
         std::cout << "Value of error on sigma: " << fitErrorFunction->GetParError(2) << std::endl;
+        return fitErrorFunction;
+
+}
+
+void fit_efficiencies(){
+    // suppose there exists a root file containing a TClonesArray of Efficiency Curves
+    TFile* efficiencyCurveFile = TFile::Open("efficiencyCurves.root");
+    // how many efficiency curves to fit
+    Int_t numberSlices = 7;
+    // initialize TClonesArray to hold the efficiency curves
+    TClonesArray* efficiencyArray = new TClonesArray( "TEfficiency", numberSlices );
+    // initialize TClonesArray to hold the fit objects 
+    TClonesArray* efficiencyFitArray = new TClonesArray( "TF1", numberSlices );
+
+    for ( int i = 0 ; i < numberSlices ; i++){
+
 
         
     }
