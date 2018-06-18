ec3f016a49cca371aa322198dde3c1f2eede7c29
Joseph <josephcorrado7@gmail.com>
2018-06-18 13:16:12 -0400

    Changed from using the total ntuple over 3 years, to simply adding the three files to a tchain separately

--- Mu_Analysis/plotMETDistsVersusMu/Run_Proof_Driver.C
+++ Mu_Analysis/plotMETDistsVersusMu/Run_Proof_Driver.C
@@ -1,7 +1,7 @@
 {
     TProof::Open("");
     TChain* mychain = new TChain( "METTree" , "Jburr_CELL_MET_Dists_versus_Mu" );
-    mychain->Add("${DATA}/totalJburrTuple.root");
+    mychain->Add("${DATA}/totalntuple201\d.root");
     mychain->SetProof();
     mychain->Process("PlotMETDistsVersMu.C+");
 }
