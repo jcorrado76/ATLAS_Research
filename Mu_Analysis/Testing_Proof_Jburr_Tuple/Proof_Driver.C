67e6dff5f3b279a1d389ab82a9539e614090ad20
Joseph <josephcorrado7@gmail.com>
2018-06-18 12:49:27 -0400

    Deleted proof parallel testing facilities

--- Mu_Analysis/Testing_Proof_Jburr_Tuple/Proof_Driver.C
+++
@@ -1,7 +0,0 @@
-{
-    TProof::Open("");
-    TChain* mychain = new TChain( "METTree" , "Jburr_CELL_MET_Dists_versus_Mu" );
-    mychain->Add("${DATA}/totalJburrTuple.root");
-    mychain->SetProof();
-    mychain->Process("PlotInParallel.C+");
-}
