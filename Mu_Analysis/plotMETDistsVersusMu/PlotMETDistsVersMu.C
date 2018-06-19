966a6d5ea78fc38db98f3f63b329cea1bbf7b3ff
Joseph <josephcorrado7@gmail.com>
2018-06-19 12:17:56 -0400

    Axis labels must go on the THStack object

--- Mu_Analysis/plotMETDistsVersusMu/PlotMETDistsVersMu.C
+++ Mu_Analysis/plotMETDistsVersusMu/PlotMETDistsVersMu.C
@@ -60,10 +60,12 @@ void PlotMETDistsVersMu::SlaveTerminate()
 
 void PlotMETDistsVersMu::Terminate()
 {
-    TCanvas* mycanv = new TCanvas("MET_AlgMuSlices", "MET Slices in Mu");
-    THStack* muSlicesStack = new THStack("muStack","MET Hists in Mu Slices After Removing Runs 33023, 331975, and 334487");
     TString xaxisTitle = alg_name + " [GeV]";
     TString yaxisTitle = "Number of Entries";
+    TCanvas* mycanv = new TCanvas("MET_AlgMuSlices", "MET Slices in Mu");
+    THStack* muSlicesStack = new THStack("muStack","MET Hists in Mu Slices After Removing Runs 33023, 331975, and 334487");
+    muSlicesStack->GetXaxis()->SetTitle( xaxisTitle );
+    muSlicesStack->GetYaxis()->SetTitle( yaxisTitle );
 
     // color the histograms so we can see difference 
     MET_Datamu0thru10->SetLineColor(1);
@@ -74,8 +76,6 @@ void PlotMETDistsVersMu::Terminate()
     MET_Datamu50thru60->SetLineColor(6);
     MET_Datamu60thru70->SetLineColor(9);
 
-    MET_Datamu0thru10->GetXaxis()->SetTitle( xaxisTitle );
-    MET_Datamu0thru10->GetYaxis()->SetTitle( yaxisTitle );
 
     // normalize histograms to 1 so we can compare shapes 
     Int_t MET_Datamu0thru10Nentries = MET_Datamu0thru10->GetEntries();
