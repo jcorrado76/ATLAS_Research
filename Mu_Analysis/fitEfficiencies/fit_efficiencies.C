97eeb2ccd6bba3406cb7ab4cbe30e2202573600a
Joseph <josephcorrado7@gmail.com>
2018-06-28 12:15:53 -0400

    Put every line inside of a try catch block to safeguard against problems

--- Mu_Analysis/fitEfficiencies/fit_efficiencies.C
+++ Mu_Analysis/fitEfficiencies/fit_efficiencies.C
@@ -1,12 +1,32 @@
 #include "fittingRoutine.h"
+#include <iostream>
 
 {
     TString filePath = "../computeL1XE30Efficiency/EfficiencyFits.root";
-    
-    FittingRoutine* MyFittingRoutine = new FittingRoutine();
-    MyFittingRoutine->getTEfficiencyObjects( filePath );
-    MyFittingRoutine->fit_efficiencies();
-    MyFittingRoutine->writeFitsToFile();
+    try{
+        FittingRoutine* MyFittingRoutine = new FittingRoutine();
+    }
+    catch(){
+        std::cout << "Failed to initialize the fittingroutine object" << std::endl;
+    }
+    try{
+        MyFittingRoutine->getTEfficiencyObjects( filePath );
+    }
+    catch)_{
+        std::cout << "Failed to get the tefficiency objects from: " << filePath << std::endl;
+    }
+    try{
+        MyFittingRoutine->fit_efficiencies();
+    }
+    catch(){
+        std::cout << "Something failed inside fit_efficiencies" << std::endl;
+    }
+    try{
+        MyFittingRoutine->writeFitsToFile();
+    }
+    catch() {
+        std::cout << "Failed to write new TF1 fits to file" << std::endl;
+    }«»
 
 }
 
