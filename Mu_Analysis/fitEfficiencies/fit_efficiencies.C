766dfe648f7d7d803bf4b9844b49221460f96f30
Joseph <josephcorrado7@gmail.com>
2018-06-28 14:02:39 -0400

    Moved closing the file to after last access to the efficiency objects, or else they would not be available

--- Mu_Analysis/fitEfficiencies/fit_efficiencies.C
+++ Mu_Analysis/fitEfficiencies/fit_efficiencies.C
@@ -50,7 +50,6 @@ void fit_efficiencies(){
     TEfficiency*  MET_Algmu40thru50Efficiency = (TEfficiency*)myfile->Get("metmu40thru50Efficiency");
     TEfficiency*  MET_Algmu50thru60Efficiency = (TEfficiency*)myfile->Get("metmu50thru60Efficiency");
     TEfficiency*  MET_Algmu60thru70Efficiency = (TEfficiency*)myfile->Get("metmu60thru70Efficiency");
-    myfile->Close();
 
     TF1* mu0thru10FitFunction = generateFitFunction( MET_Algmu0thru10Efficiency);
     TF1* mu10thru20FitFunction = generateFitFunction( MET_Algmu10thru20Efficiency);
@@ -59,6 +58,7 @@ void fit_efficiencies(){
     TF1* mu40thru50FitFunction = generateFitFunction( MET_Algmu40thru50Efficiency);
     TF1* mu50thru60FitFunction = generateFitFunction( MET_Algmu50thru60Efficiency);
     TF1* mu60thru70FitFunction = generateFitFunction( MET_Algmu60thru70Efficiency);
+    myfile->Close();
     TFile* FitFile = TFile::Open( efficiencyFitsFilePath , "RECREATE" );
     FitFile->Close();
     
