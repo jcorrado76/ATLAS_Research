dc3fe03e80a4796eb1021cc6e9ab79099aa25be7
Joseph Corrado <jgc375@nyu.edu>
2018-07-11 10:21:57 -0400

    Wrote the function to compute the weights using the HLT no alg L1 XE prescale in numerator, and the denominator the efficiency of the L1 XE 30 evaluated at the current value of L1 MET

--- Mu_Analysis/PerformCorrection/CorrectingDistributions.C
+++ Mu_Analysis/PerformCorrection/CorrectingDistributions.C
@@ -16,38 +16,42 @@ Bool_t CorrectingDistributions::Process(Long64_t entry)
 {
    fReader.SetEntry(entry);
 
-   if ( passedL1ZB()  && isGoodRun() ){
+   if ( isPassnoAlgL1XE30() && isGoodRun() ){
        if ( inMuRange( 0.0 , 10.0) ){
-           MET_Correctedmu0thru10->Fill(*MET_Data, );
+           MET_Correctedmu0thru10->Fill(*MET_Data, ComputeWeight(EfficiencyFitMuBin1));
        }
        if ( inMuRange( 10.0, 20.0) ){
-           MET_Correctedmu10thru20->Fill(*MET_Data,);
+           MET_Correctedmu10thru20->Fill(*MET_Data,ComputeWeight(EfficiencyFitMuBin2));
        }
        if ( inMuRange( 20.0 , 30.0) ){
-           MET_Correctedmu20thru30->Fill(*MET_Data,);
+           MET_Correctedmu20thru30->Fill(*MET_Data,ComputeWeight(EfficiencyFitMuBin3));
        }
        if ( inMuRange( 30.0, 40.0) ){
-           MET_Correctedmu30thru40->Fill(*MET_Data,);
+           MET_Correctedmu30thru40->Fill(*MET_Data,ComputeWeight(EfficiencyFitMuBin4));
        }
        if ( inMuRange( 40.0 , 50.0 ) ){
-           MET_Correctedmu40thru50->Fill(*MET_Data,);
+           MET_Correctedmu40thru50->Fill(*MET_Data,ComputeWeight(EfficiencyFitMuBin5));
        }
        if ( inMuRange( 50.0 , 60.0 ) ){
-           MET_Correctedmu50thru60->Fill(*MET_Data,);
+           MET_Correctedmu50thru60->Fill(*MET_Data,ComputeWeight(EfficiencyFitMuBin6));
        }
        if ( inMuRange( 60.0 , 70.0 ) ){
-           MET_Correctedmu60thru70->Fill(*MET_Data,);
+           MET_Correctedmu60thru70->Fill(*MET_Data,ComputeWeight(EfficiencyFitMuBin7));
        }
    }
 
    return kTRUE;
 }
-Double CorrectingDistributions::ComputeWeight(TF1* fitFunc )
+Double_t CorrectingDistributions::ComputeWeight(TF1* fitFunc)
 {
-
+    Float_t numerator = *HLT_noalg_L1XE30_prescale;
+    Double_t denominator = fitFunc->Eval( *cell_met ); 
+    return numerator / denominator; 
 }
 void CorrectingDistributions::SlaveTerminate(){}
 void CorrectingDistributions::Terminate(){}
+void CorrectingDistributions::Init(TTree *tree){fReader.SetTree(tree);}
+Bool_t CorrectingDistributions::Notify(){return kTRUE;}
 Bool_t PlotMETDistsVersMu::isGoodRun(){//{{{
     return (*RunNumber != 330203 && *RunNumber != 331975 && *RunNumber != 334487);
 }
@@ -58,3 +62,6 @@ Bool_t PlotMETDistsVersMu::passedL1ZB(){//{{{
 Bool_t PlotMETDistsVersMu::inMuRange( Float_t a , Float_t b ){ //{{{
     return ( *InTimePileup > a && *InTimePileup < b );
 } //}}}
+Bool_t L1XEefficiencySelector::isPassnoAlgL1XE30(){ // {{{
+    return (*HLT_noalg_L1XE30_passed);
+} // }}}
--- Mu_Analysis/PerformCorrection/CorrectingDistributions.h
+++ Mu_Analysis/PerformCorrection/CorrectingDistributions.h
@@ -65,7 +65,7 @@ public :
    virtual void    Terminate();
    Bool_t isGoodRun();
    Bool_t passedL1ZB();
-   Bool_t isPassnoAlgXE30();
+   Bool_t isPassnoAlgL1XE30();
    Bool_t inMuRange( Float_t , Float_t );
    Double_t ComputeWeight(TF1*);
 
@@ -75,29 +75,3 @@ public :
 
 #endif
 
-#ifdef CorrectingDistributions_cxx
-void CorrectingDistributions::Init(TTree *tree)
-{
-   // The Init() function is called when the selector needs to initialize
-   // a new tree or chain. Typically here the reader is initialized.
-   // It is normally not necessary to make changes to the generated
-   // code, but the routine can be extended by the user if needed.
-   // Init() will be called many times when running on PROOF
-   // (once per file to be processed).
-
-   fReader.SetTree(tree);
-}
-
-Bool_t CorrectingDistributions::Notify()
-{
-   // The Notify() function is called when a new file is opened. This
-   // can be either for a new TTree in a TChain or when when a new TTree
-   // is started when using PROOF. It is normally not necessary to make changes
-   // to the generated code, but the routine can be extended by the
-   // user if needed. The return value is currently not used.
-
-   return kTRUE;
-}
-
-
-#endif // #ifdef CorrectingDistributions_cxx
