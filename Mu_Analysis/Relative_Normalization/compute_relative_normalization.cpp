4b5d58197fbf0201acebe5385f6519f1f37cc3be
Joseph Corrado <jgc375@nyu.edu>
2018-09-01 19:47:01 -0400

    Declared the histograms to store the met distributions in

--- Mu_Analysis/Relative_Normalization/compute_relative_normalization.cpp
+++ Mu_Analysis/Relative_Normalization/compute_relative_normalization.cpp
@@ -5,20 +5,29 @@ void compute_relative_normalization(){
 
     TFile* mu_file = TFile::Open("../Root_Files/mu_analysis.root");
 
-    if ( mu_analysis_file->GetDirectory("relative_normalized_curves"){
+
+    TH1F* metdistmubin1 = new TH1F("h1","mubin1");
+    TH1F* metdistmubin2 = new TH1F("h2","mubin2");
+    TH1F* metdistmubin3 = new TH1F("h3","mubin3");
+    TH1F* metdistmubin4 = new TH1F("h4","mubin4");
+    TH1F* metdistmubin5 = new TH1F("h5","mubin5");
+    TH1F* metdistmubin6 = new TH1F("h6","mubin6");
+    TH1F* metdistmubin7 = new TH1F("h7","mubin7");
+
+    if ( mu_analysis_file->GetDirectory( relative_normalization_directory_name ){
         TDirectory* relative_norm_dir = mu_analysis_file->GetDirectory( relative_normalization_directory_name );
 
-        efficiency_dir->GetObject("metmu0thru10Efficiency",efficiencyObjectMu0thru10);
-        efficiency_dir->GetObject("metmu10thru20Efficiency",efficiencyObjectMu10thru20);
-        efficiency_dir->GetObject("metmu20thru30Efficiency",efficiencyObjectMu20thru30);
-        efficiency_dir->GetObject("metmu30thru40Efficiency",efficiencyObjectMu30thru40);
-        efficiency_dir->GetObject("metmu40thru50Efficiency",efficiencyObjectMu40thru50);
-        efficiency_dir->GetObject("metmu50thru60Efficiency",efficiencyObjectMu50thru60);
-        efficiency_dir->GetObject("metmu60thru70Efficiency",efficiencyObjectMu60thru70);
+        relative_norm_dir->GetObject("",metdistmubin1);
+        relative_norm_dir->GetObject("",metdistmubin2);
+        relative_norm_dir->GetObject("",metdistmubin3);
+        relative_norm_dir->GetObject("",metdistmubin4);
+        relative_norm_dir->GetObject("",metdistmubin5);
+        relative_norm_dir->GetObject("",metdistmubin6);
+        relative_norm_dir->GetObject("",metdistmubin7);
 
     }else{
         std::cout << "Relative normalization directory already exsits" << std::endl;
         TDirectory* relative_norm_dir = mu_analysis_file->mkdir( relative_normalization_directory_name );
         std::cout << "Successfully got the relative normalization directory" << std::endl;
-}
+    }
 
