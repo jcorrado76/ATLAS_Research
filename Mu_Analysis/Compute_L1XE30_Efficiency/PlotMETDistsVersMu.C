1a63aef0fd24b5e9408eb04eb12afb3d2b695701
Joseph Corrado <jgc375@nyu.edu>
2018-10-18 15:10:00 -0400

    Moved some of the folders around. Made some better names. Started trying to use an std vector to loop over the histogram objects. But now I'm getting a bus error

---
+++ Mu_Analysis/Compute_L1XE30_Efficiency/PlotMETDistsVersMu.C
@@ -0,0 +1,139 @@
+#define PlotMETDistsVersMu_cxx
+
+
+#include "PlotMETDistsVersMu.h"
+
+void PlotMETDistsVersMu::Begin(TTree *) // {{{
+{
+   TString option = GetOption();
+   // fill Mu_Values
+   for (int i = 0 ; i < Number_Mu_Bins + 1; i++){
+       Mu_Values[i] = i * 10.;
+   }
+   int n;
+   Float_t muLow;
+   Float_t muHigh;
+   TString Name;
+   TString Title;
+   TString EfficiencyName;
+   TString EfficiencyTitle;
+   for (int i = 0 ; i < Number_Mu_Bins-1 ; i++){
+       muLow = Mu_Values[i+1];
+       muHigh = Mu_Values[i+2];
+       Name.Form("metmu%.0fthru%.0f" , muLow , muHigh );
+       EfficiencyName.Form("metmu%.0fthru%.0fEfficiency", muLow , muHigh );
+       Title.Form("ZeroBias MET Distribution for %s With Actint Between %.0f and %.0f" ,Alg_Name.Data(), muLow , muHigh );
+       EfficiencyTitle.Form("Efficiency of L1XE 30 As a Function of %s for Actint Between %.0f and %.0f", Alg_Name.Data() , muLow , muHigh );
+
+       MET_Distribution = new TH1F( Name , Title , met_dist_nbins , gevLow , gevHigh );
+       Met_Distributions_By_Mu_Bin.push_back( MET_Distribution );
+
+       MET_L1XE30Efficiency = new TEfficiency( EfficiencyName , EfficiencyTitle , efficiency_nbins , gevLow , gevHigh );
+       L1XE30_Efficiency_Objects.push_back( MET_L1XE30Efficiency );
+
+       MET_L1XE30EfficiencyFit = new TF1();
+       L1XE30_Efficiency_Fit_Objects.push_back( MET_L1XE30EfficiencyFit );
+   }
+} // }}}
+Bool_t PlotMETDistsVersMu::Process(Long64_t entry)//{{{
+{
+   fReader.SetLocalEntry(entry);
+
+   // if the event has passed HLT_noalg_zb_L1ZB_passed, and is not one of the bad run numbers
+   if ( isHLT_zb_L1ZB()  && isGoodRun() ){
+       Float_t muLow, muHigh;
+       for ( int i = 0; i < Number_Mu_Bins ; i++ ){
+           muLow = Mu_Values[ i ];
+           muHigh = Mu_Values[ i + 1 ];
+           if ( inMuRange( muLow , muHigh ) ){
+               for ( int j = 0 ; j < Number_Mu_Bins ; j++ ){
+                   (Met_Distributions_By_Mu_Bin[j])->Fill(*MET_Data , *HLT_noalg_zb_L1ZB_prescale);
+                   (L1XE30_Efficiency_Objects[j])->Fill(*L1_MET > XE , *MET_Data);
+               }
+           }
+       }
+   }
+   return kTRUE;
+}//}}}
+void PlotMETDistsVersMu::Terminate() //{{{
+{
+   for ( int i = 0; i < Number_Mu_Bins ; i++ ){
+       (L1XE30_Efficiency_Objects[i])->SetLineColor( Colors[i] );
+       (L1XE30_Efficiency_Objects[i])->SetMarkerStyle( Colors[i] );
+       (L1XE30_Efficiency_Fit_Objects[i]) = generateFitFunction( L1XE30_Efficiency_Objects[i] );
+       (L1XE30_Efficiency_Fit_Objects[i])->SetLineColor( Colors[i] );
+       (Met_Distributions_By_Mu_Bin[i])->SetLineColor( Colors[i] );
+       Normalized_Met_Distributions.push_back( (TH1F*)(Met_Distributions_By_Mu_Bin[i])->Clone());
+       (Normalized_Met_Distributions[i])->SetNormFactor(1.);
+   }
+    // WRITE TO FILE {{{
+    TFile* Mu_Analysis_File = TFile::Open("mu_analysis.root", "RECREATE");
+    TDirectory* zb_met_distributions = Mu_Analysis_File->mkdir("zb_met_distributions");
+    zb_met_distributions->cd();
+    for ( int  i = 0 ; i < Number_Mu_Bins ; i++ ){
+        (Met_Distributions_By_Mu_Bin[i])->Write();
+    }
+
+    TDirectory* efficiency_curves = Mu_Analysis_File->mkdir("l1xe30_efficiency_curves");
+    efficiency_curves->cd();
+    for ( int  i = 0 ; i < Number_Mu_Bins ; i++ ){
+        (L1XE30_Efficiency_Objects[i])->Write();
+    }
+
+    TDirectory* L1XE30_Efficiency_Fit_Objects_Dir = Mu_Analysis_File->mkdir("l1xe30_efficiency_fits");
+    L1XE30_Efficiency_Fit_Objects_Dir->cd();
+    for ( int  i = 0 ; i < Number_Mu_Bins ; i++ ){
+        (L1XE30_Efficiency_Fit_Objects[i])->Write();
+    }
+
+    Mu_Analysis_File->Close();//}}}
+}//}}}
+void PlotMETDistsVersMu::SlaveBegin(TTree * /*tree*/) // {{{
+{
+   TString option = GetOption();
+} // }}}
+void PlotMETDistsVersMu::SlaveTerminate(){}
+void PlotMETDistsVersMu::Init(TTree *tree) //{{{
+{
+   fReader.SetTree(tree);
+} //}}}
+Bool_t PlotMETDistsVersMu::Notify() //{{{
+{
+   return kTRUE;
+} //}}}
+Bool_t PlotMETDistsVersMu::isGoodRun(){ //{{{
+    return (*RunNumber != 330203 && *RunNumber != 331975 && *RunNumber != 334487);
+} // }}}
+Bool_t PlotMETDistsVersMu::isHLT_zb_L1ZB(){ //{{{
+    return (*HLT_noalg_zb_L1ZB_passed);
+} //}}}
+Bool_t PlotMETDistsVersMu::isHLT_zb_L1XE30(){ //{{{
+    return (*HLT_noalg_L1XE30_passed);
+} //}}}
+Bool_t PlotMETDistsVersMu::inMuRange( Float_t a , Float_t b ){ //{{{
+    return ( *InTimePileup > a && *InTimePileup < b );
+} //}}}
+Double_t PlotMETDistsVersMu::fitFunction(Double_t *x , Double_t *par ){//{{{
+    Float_t xx = x[0];
+    Double_t l1cut = 30.0;
+    Double_t fitval = (1./2.)*(1.+TMath::Erf((par[0]*x[0]+par[1]-l1cut)/(par[2]*TMath::Sqrt(2.))));
+    return fitval;
+}//}}}
+TF1* PlotMETDistsVersMu::generateFitFunction(TEfficiency* teff_obj, float gevMax, float initial_slope, float initial_intercept, float initial_sigma ){//{{{
+    TF1 *fitErrorFunction = new TF1("fitFunction",fitFunction,0.0,gevMax,3);
+    fitErrorFunction->SetParameter(0, initial_slope);
+    fitErrorFunction->SetParameter(1, initial_intercept);
+    fitErrorFunction->SetParameter(2, initial_sigma);
+    //initializing parameters reasonably is important because it is a maximum likelihood fit
+    fitErrorFunction->SetParNames("Slope","Translation","Sigma");
+    //"R" tells the fit function from BinomialEfficiency::Fit to use the range of the TF1 as the fitting range
+    teff_obj->Fit( fitErrorFunction  , "R+");
+
+    std::cout << "Value of fit for a: " << fitErrorFunction->GetParameter(0) << std::endl;
+    std::cout << "Value of error on a: " << fitErrorFunction->GetParError(0) << std::endl;
+    std::cout << "Value of fit for b: " << fitErrorFunction->GetParameter(1) << std::endl;
+    std::cout << "Value of error on b: " << fitErrorFunction->GetParError(1) << std::endl;
+    std::cout << "Value of fit for sigma: " << fitErrorFunction->GetParameter(2) << std::endl;
+    std::cout << "Value of error on sigma: " << fitErrorFunction->GetParError(2) << std::endl;
+    return fitErrorFunction;
+}//}}}
--- Mu_Analysis/Compute_L1XE30_Efficiency/PlotMETDistsVersMu.h
+++ Mu_Analysis/Compute_L1XE30_Efficiency/PlotMETDistsVersMu.h
@@ -27,20 +27,14 @@ class PlotMETDistsVersMu : public TSelector {
 public :
    TTreeReader     fReader;  //!the tree reader
    TTree          *fChain = 0;   //!pointer to the analyzed TTree or TChain
-   TH1D*            MET_Datamu0thru10 = 0;
-   TH1D*            MET_Datamu10thru20 = 0;
-   TH1D*            MET_Datamu20thru30 = 0;
-   TH1D*            MET_Datamu30thru40 = 0;
-   TH1D*            MET_Datamu40thru50 = 0;
-   TH1D*            MET_Datamu50thru60 = 0;
-   TH1D*            MET_Datamu60thru70 = 0;
-   TEfficiency*            MET_Algmu0thru10Efficiency = 0;
-   TEfficiency*            MET_Algmu10thru20Efficiency = 0;
-   TEfficiency*            MET_Algmu20thru30Efficiency = 0;
-   TEfficiency*            MET_Algmu30thru40Efficiency = 0;
-   TEfficiency*            MET_Algmu40thru50Efficiency = 0;
-   TEfficiency*            MET_Algmu50thru60Efficiency = 0;
-   TEfficiency*            MET_Algmu60thru70Efficiency = 0;
+
+   std::vector<TH1F*> Met_Distributions_By_Mu_Bin;
+   std::vector<TH1F*> Normalized_Met_Distributions;
+   std::vector<TEfficiency*> L1XE30_Efficiency_Objects;
+   std::vector<TF1*> L1XE30_Efficiency_Fit_Objects;
+   TH1F* MET_Distribution = 0;
+   TEfficiency* MET_L1XE30Efficiency = 0;
+   TF1* MET_L1XE30EfficiencyFit = 0;
 
 
     Float_t gevLow = 0.0;
@@ -49,11 +43,16 @@ public :
     Float_t efficiency_bin_width = 20.0; //want 20 GeV bins
     Int_t met_dist_nbins = (gevHigh - gevLow) / met_dist_binwidth; // compute nbins as function of preferred width
     Int_t efficiency_nbins = (gevHigh - gevLow) / efficiency_bin_width;
+    const static Int_t Number_Mu_Bins = 7;
 
+   // initialize all entries to 0
+   Float_t Mu_Values[Number_Mu_Bins+1] = { 0.0 };
 
-
+   Int_t Colors[Number_Mu_Bins] = {1,2,3,4,12,6,9};
+   Int_t MarkerStyles[Number_Mu_Bins] = {29,20,3,4,27,22,21};
 
    Float_t XE = 30.0; // L1 cut
+   TString Alg_Name = "CELL MET";
    TString zb_alg_name = "cell.met"; 
    TString l1_alg_name = "L1.met";
     // set titles and axis labels. 
@@ -97,7 +96,6 @@ public :
    Bool_t inMuRange( Float_t , Float_t );
    static Double_t fitFunction(Double_t *x , Double_t *par );
    TF1* generateFitFunction(TEfficiency* teff_obj, float gevMax = 300.0, float initial_slope = 0.1 , float initial_intercept = 0.0, float initial_sigma = 10.0);
-   TEfficiency* GetEfficiency();
 
    ClassDef(PlotMETDistsVersMu,0);
 
