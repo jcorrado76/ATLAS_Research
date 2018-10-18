d2272467425f0c57ee99e5243e778704de6cf005
Joseph Corrado <jgc375@nyu.edu>
2018-10-18 15:21:51 -0400

    Also told driver to force recompilation every time

--- Mu_Analysis/Compute_L1XE30_Efficiency/run_plotMETDist.C
+++ Mu_Analysis/Compute_L1XE30_Efficiency/run_plotMETDist.C
@@ -1,6 +1,6 @@
 #include "./PlotMETDistsVersMu.h"
 void run_plotMETDist(){
-    PlotMETDistsVersMu* myAnalysis = (PlotMETDistsVersMu*)TSelector::GetSelector("PlotMETDistsVersMu.C+g");
+    PlotMETDistsVersMu* myAnalysis = (PlotMETDistsVersMu*)TSelector::GetSelector("PlotMETDistsVersMu.C++g");
     TChain* mychain = new TChain( "METTree" , "mychain" );
     mychain->Add("~/DATA/ZB/user.jburr.2017_11_17.ZB/*");
     mychain->Process(myAnalysis);
