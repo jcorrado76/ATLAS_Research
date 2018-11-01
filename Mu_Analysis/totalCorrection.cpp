61580e10256d92f0a7d3e08993bcc433f4b5f1ac
Joseph Corrado <jgc375@nyu.edu>
2018-11-01 11:58:36 -0400

    Fixed typo in the include

--- Mu_Analysis/totalCorrection.cpp
+++ Mu_Analysis/totalCorrection.cpp
@@ -1,5 +1,5 @@
-#include "zbToL1XE30/plotMETDistsVersusMu/PlotMETDistsVersMu.h"
-#include "zbToL1XE30/PerformCorrection/CorrectingDistributions.h"
+#include "Compute_L1XE30_Efficiency/ComputeL1XE30toZBEfficiency.h"
+#include "CorrectL1XE30toZB/CorrectL1XE30toZB.h"
 #include "L1XE30toL1XE50/plotl130dists/BuildEfficiencyL150.h"
 #include "L1XE30toL1XE50/performL130to50Correction/CorrectL1XE50ToZB.h"
 #include <TChain.h>
@@ -7,21 +7,21 @@
 #include <TEfficiency.h>
 
 int totalCorrection(){
-    //PlotMETDistsVersMu* buildl1xe30Efficiencies = (PlotMETDistsVersMu*)TSelector::GetSelector("zbToL1XE30/plotMETDistsVersusMu/PlotMETDistsVersMu.C+");
-    //TChain* zb_chain = new TChain( "METTree" , "zb_chain" );
-    //zb_chain->Add("~/DATA/ZB/user.jburr.2017_11_17.ZB/*");
-    //zb_chain->Process(buildl1xe30Efficiencies);
+    ComputeL1XE30toZBEfficiency* buildl1xe30Efficiencies = (ComputeL1XE30toZBEfficiency*)TSelector::GetSelector("Compute_L1XE30_Efficiency/ComputeL1XE30toZBEfficiency.C+");
+    TChain* zb_chain = new TChain( "METTree" , "zb_chain" );
+    zb_chain->Add("~/DATA/ZB/user.jburr.2017_11_17.ZB/*");
+    zb_chain->Process(buildl1xe30Efficiencies);
 
-    //CorrectingDistributions* correctToZB = (CorrectingDistributions*) TSelector::GetSelector("zbToL1XE30/PerformCorrection/CorrectingDistributions.C+");
+    CorrectL1XE30toZB* correctToZB = (CorrectL1XE30toZB*) TSelector::GetSelector("CorrectL1XE30toZB/CorrectL1XE30toZB.C+");
     TChain* jetm10_chain = new TChain( "METTree", "jetm10chain");
     jetm10_chain->Add("~/DATA/PhysicsMain/JburrJETM10*");
-    //jetm10_chain->Process(correctToZB);
+    jetm10_chain->Process(correctToZB);
 
     //BuildEfficiencyL150* createL1XE50Efficiencies = (BuildEfficiencyL150*) TSelector::GetSelector("L1XE30toL1XE50/plotl130dists/BuildEfficiencyL150.C+");
     //jetm10_chain->Process( createL1XE50Efficiencies );
 
-    CorrectL1XE50ToZB* CorrectL1XE50DataAllWay = (CorrectL1XE50ToZB*) TSelector::GetSelector("L1XE30toL1XE50/performL130to50Correction/CorrectL1XE50ToZB.C+");
-    jetm10_chain->Process( CorrectL1XE50DataAllWay );
+    //CorrectL1XE50ToZB* CorrectL1XE50DataAllWay = (CorrectL1XE50ToZB*) TSelector::GetSelector("L1XE30toL1XE50/performL130to50Correction/CorrectL1XE50ToZB.C+");
+    //jetm10_chain->Process( CorrectL1XE50DataAllWay );
 
 
     return(1);
