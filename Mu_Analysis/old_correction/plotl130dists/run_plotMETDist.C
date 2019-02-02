b84631b113bdb46e5cc268cf75d64c6641970fda
Joseph Corrado <jgc375@nyu.edu>
2019-02-02 12:19:01 -0500

    should honestly just get rid of things called 'old' or 'decprecated' as I can always recover them and they clutter up my project

--- Mu_Analysis/old_correction/plotl130dists/run_plotMETDist.C
+++
@@ -1,8 +0,0 @@
-#include "./PlotMETDistsVersMu.h"
-void run_plotMETDist(){
-    PlotMETDistsVersMu* myAnalysis = (PlotMETDistsVersMu*)TSelector::GetSelector("PlotMETDistsVersMu.C+");
-    TChain* mychain = new TChain( "METTree" , "mychain" );
-    mychain->Add("~/DATA/PhysicsMain/user.jburr.2017_11_17.JETM10/*");
-    mychain->Process(myAnalysis);
-}
-
