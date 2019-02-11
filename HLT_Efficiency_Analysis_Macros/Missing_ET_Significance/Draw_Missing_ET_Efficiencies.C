dd10e2da373bc5a55b37954f4ba7e88f0df595de
Joseph Corrado <jgc375@nyu.edu>
2019-02-11 12:05:51 -0500

    working on getting the right names for the branches into the selector to plot the efficiency

--- HLT_Efficiency_Analysis_Macros/Missing_ET_Significance/Draw_Missing_ET_Efficiencies.C
+++ HLT_Efficiency_Analysis_Macros/Missing_ET_Significance/Draw_Missing_ET_Efficiencies.C
@@ -22,15 +22,14 @@ Bool_t Draw_Missing_ET_Efficiencies::Process(Long64_t entry)
     isClean = (*passcleancuts > 0.1) && ( *recalbroke < 0.1);
     passTransverseMassCut = Efficiency_Lib::passTransverseMassCut( *metoffrecal , *mexoffrecal , *meyoffrecal , *metoffrecalmuon , *mexoffrecalmuon , *meyoffrecalmuon );
 
-    if ( isMuon && isClean && ( muonActint > actintCut ))
+    if ( isMuon && isClean && Efficiency_Lib::passTransverseMassCut(metoffrecal,mexoffrecal,meyoffrecal,metoffrecalmuon,mexoffrecalmuon,meyoffrecalmuon) && ( muonActint > actintCut ) )
     {
-        if ( Efficiency_Lib::passTransverseMassCut(metoffrecal,mexoffrecal,meyoffrecal,metoffrecalmuon,mexoffrecalmuon,meyoffrecalmuon) )
-        {
-            Float_t metnomu = Efficiency_Lib::computeMetNoMu(  mexoffrecal , meyoffrecal , mexoffrecalmuon , meyoffrecalmuon );
-    Ateff->Fill( (algAmuonMET > AlgAIndividThresh ) && (muonMetl1 > metl1thresh) && ( muonActint > actintCut ), metnomu );
-    Bteff->Fill( (algBmuonMET > AlgBIndividThresh ) && (muonMetl1 > metl1thresh) && ( muonActint > actintCut ), metnomu );
-    Cteff->Fill( ( (algAmuonMET > CombinedThreshAlgA) && (algBmuonMET > CombinedThreshAlgB )
-    && ( muonActint > actintCut ) && (muonMetl1 > metl1thresh) ), metnomu );
+        Float_t metnomu = Efficiency_Lib::computeMetNoMu(  mexoffrecal , meyoffrecal , mexoffrecalmuon , meyoffrecalmuon );
+        Ateff->Fill( (algAmuonMET > AlgAIndividThresh ) && (muonMetl1 > metl1thresh) && ( muonActint > actintCut ), metnomu );
+        Bteff->Fill( (algBmuonMET > AlgBIndividThresh ) && (muonMetl1 > metl1thresh) && ( muonActint > actintCut ), metnomu );
+        Cteff->Fill( ( (algAmuonMET > CombinedThreshAlgA) && (algBmuonMET > CombinedThreshAlgB )
+        && ( muonActint > actintCut ) && (muonMetl1 > metl1thresh) ), metnomu );
+    }
    return kTRUE;
 }
 void Draw_Missing_ET_Efficiencies::SlaveTerminate(){}
