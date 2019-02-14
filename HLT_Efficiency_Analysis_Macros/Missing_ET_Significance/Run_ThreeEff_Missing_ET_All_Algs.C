104df57da799e8875978967be1da6ef15793564e
Joseph Corrado <jgc375@nyu.edu>
2019-02-14 15:16:09 -0500

    figured out why the run all alg combs macro wasn't working

--- HLT_Efficiency_Analysis_Macros/Missing_ET_Significance/Run_ThreeEff_Missing_ET_All_Algs.C
+++ HLT_Efficiency_Analysis_Macros/Missing_ET_Significance/Run_ThreeEff_Missing_ET_All_Algs.C
@@ -4,9 +4,9 @@ Int_t Run_ThreeEff_Missing_ET_All_Algs( const TString& folderName = "")
 {
     TString algArray[5] = {"metcell","metmht","mettopocl","mettopoclps","mettopoclpuc"};
     
-    for ( std::size_t i = i+1 ; i < sizeof(algArray) ; i++)
+    for ( std::size_t i = 0 ; i < sizeof(algArray) ; i++)
     {
-        gROOT->ProcessLine(".x Perform_Missing_ET_Efficiency_Analysis.C(\"" + algArray[i] + "\");");
+        gROOT->ProcessLine(".x Missing_ET_Significance/Perform_Missing_ET_Efficiency_Analysis.C(\"" + algArray[i] + "\");");
     }
 
   return(0);
