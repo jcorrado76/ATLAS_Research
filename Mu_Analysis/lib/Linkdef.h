4af40164cc935a867bfbc53da8d21cf066e6f94a
Joseph Corrado <jgc375@nyu.edu>
2019-02-26 16:37:42 -0500

    removed defined in calls for linkdef

--- Mu_Analysis/lib/Linkdef.h
+++ Mu_Analysis/lib/Linkdef.h
@@ -2,12 +2,6 @@
 #pragma link off al globals;
 #pragma link off all classes;
 #pragma link off all functions;
-
-//#pragma link C++ defined_in "ComputeL1XE30toZBEfficiency.h+";
-//#pragma link C++ defined_in "ComputeL1XE50toL1XE30Efficiency.h+";
-//#pragma link C++ defined_in "CorrectL1XE30toZB.h+";
-//#pragma link C++ defined_in "CorrectL1XE50toZB.h+";
-//#pragma link C++ defined_in "Jburr_Template_Selector.h+";
 #pragma link C++ class ComputeL1XE30toZBEfficiency+;
 #pragma link C++ class ComputeL1XE50toL1XE30Efficiency+;
 #pragma link C++ class CorrectL1XE30toZB+;
