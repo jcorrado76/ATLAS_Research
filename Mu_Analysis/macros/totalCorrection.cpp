efd3b381324f62b3940dd22425e0470d7afbb1c7
Joseph Corrado <jgc375@nyu.edu>
2019-03-08 15:08:26 -0500

    remembered to cast the getters for TObjArrays to a TObjarray pointer

--- Mu_Analysis/macros/totalCorrection.cpp
+++ Mu_Analysis/macros/totalCorrection.cpp
@@ -20,21 +20,21 @@
     // if successfully get the JETM10 data
     if (jetm10_chain->Add("/home/joseph/ATLAS_Research/DATA/PhysicsMain/JETM10_15thru17_with_missing_et_significance.root")){
         std::cout << "Processing CorrectL1XE30toZB..." << std::endl;
-        correctL1XE30ToZB->Set_L1XE30_Efficiency_Fit_Objects(computeL1XE30toZBEfficiency->Get_L1XE30_Efficiency_Fit_Objects());
-        correctL1XE30ToZB->Set_HLT_ZB_L1ZB_MET_Distributions_by_Mubin(computeL1XE30toZBEfficiency->Get_HLT_ZB_L1ZB_MET_Distributions_by_Mubin());
+        correctL1XE30ToZB->Set_L1XE30_Efficiency_Fit_Objects((TObjArray*)computeL1XE30toZBEfficiency->Get_L1XE30_Efficiency_Fit_Objects());
+        correctL1XE30ToZB->Set_HLT_ZB_L1ZB_MET_Distributions_by_Mubin((TObjArray*)computeL1XE30toZBEfficiency->Get_HLT_ZB_L1ZB_MET_Distributions_by_Mubin());
         jetm10_chain->Process( correctL1XE30ToZB );
 
         // create the l1xe50 to l1xe30 efficiency curves
         jetm10_chain->Process( computeL1XE50toL1XE30Efficiency );
 
         // need zb l1 zb met dist for normalization
-        CorrectL1XE50DataAllWay->Set_HLT_ZB_L1ZB_MET_Distributions_by_Mubin(computeL1XE30toZBEfficiency->Get_HLT_ZB_L1ZB_MET_Distributions_by_Mubin());
-        CorrectL1XE50DataAllWay->Set_HLT_ZB_L1XE30_MET_Distributions_by_Mubin(computeL1XE50toL1XE30Efficiency->Get_HLT_ZB_L1XE30_MET_Distributions_by_Mubin());
-        CorrectL1XE50DataAllWay->Set_L1XE30_Efficiency_Fit_Objects(computeL1XE30toZBEfficiency->Get_L1XE30_Efficiency_Fit_Objects());
-        CorrectL1XE50DataAllWay->Set_L1XE50_Efficiency_Fit_Objects(computeL1XE50toL1XE30Efficiency->Get_L1XE50_Efficiency_Fit_Objects());
-        CorrectL1XE50DataAllWay->Set_L1XE30_Efficiency_Objects(computeL1XE30toZBEfficiency->Get_L1XE30_Efficiency_Objects());
-        CorrectL1XE50DataAllWay->Set_L1XE50_Efficiency_Objects(computeL1XE50toL1XE30Efficiency->Get_L1XE50_Efficiency_Objects());
-        CorrectL1XE50DataAllWay->Set_HLT_ZB_L1XE30_Corrected_to_ZB_MET_Distribution(correctL1XE30ToZB->Get_HLT_ZB_L1XE30_Corrected_to_ZB_MET_Distribution());
+        CorrectL1XE50DataAllWay->Set_HLT_ZB_L1ZB_MET_Distributions_by_Mubin((TObjArray*)computeL1XE30toZBEfficiency->Get_HLT_ZB_L1ZB_MET_Distributions_by_Mubin());
+        CorrectL1XE50DataAllWay->Set_HLT_ZB_L1XE30_MET_Distributions_by_Mubin((TObjArray*)computeL1XE50toL1XE30Efficiency->Get_HLT_ZB_L1XE30_MET_Distributions_by_Mubin());
+        CorrectL1XE50DataAllWay->Set_L1XE30_Efficiency_Fit_Objects((TObjArray*)computeL1XE30toZBEfficiency->Get_L1XE30_Efficiency_Fit_Objects());
+        CorrectL1XE50DataAllWay->Set_L1XE50_Efficiency_Fit_Objects((TObjArray*)computeL1XE50toL1XE30Efficiency->Get_L1XE50_Efficiency_Fit_Objects());
+        CorrectL1XE50DataAllWay->Set_L1XE30_Efficiency_Objects((TObjArray*)computeL1XE30toZBEfficiency->Get_L1XE30_Efficiency_Objects());
+        CorrectL1XE50DataAllWay->Set_L1XE50_Efficiency_Objects((TObjArray*)computeL1XE50toL1XE30Efficiency->Get_L1XE50_Efficiency_Objects());
+        CorrectL1XE50DataAllWay->Set_HLT_ZB_L1XE30_Corrected_to_ZB_MET_Distribution((TObjArray*)correctL1XE30ToZB->Get_HLT_ZB_L1XE30_Corrected_to_ZB_MET_Distribution());
         // then correct the l1xe50 data back to zerobias
         std::cout << "Processing Correct L1XE50 Data to ZB..." << std::endl;
         jetm10_chain->Process( CorrectL1XE50DataAllWay );
