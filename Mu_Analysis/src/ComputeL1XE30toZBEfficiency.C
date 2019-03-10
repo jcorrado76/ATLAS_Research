f684fbc888c13d03be228ed830b68a1b3bdb9f81
Joseph Corrado <jgc375@nyu.edu>
2019-03-10 09:31:19 -0400

    removed the assignment to add a fit function to fit functions arrays and just added in the corresponding order instead

--- Mu_Analysis/src/ComputeL1XE30toZBEfficiency.C
+++ Mu_Analysis/src/ComputeL1XE30toZBEfficiency.C
@@ -22,6 +22,6 @@ void ComputeL1XE30toZBEfficiency::Terminate() //{{{
 {
     // create the fit function for the l1xe30 efficiencies
     for ( int i = 0; i < Number_Mu_Bins ; i++ ){
-    ((TF1*)L1XE30_Efficiency_Fit_Objects->At(i)) = generateFitFunction( (TEfficiency*)L1XE30_Efficiency_Objects[i] );
+        L1XE30_Efficiency_Fit_Objects->Add( generateFitFunction( ((TEfficiency*)L1XE30_Efficiency_Objects->At(i)) ));
     }
 }//}}}
