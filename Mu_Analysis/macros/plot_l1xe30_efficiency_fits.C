f83ee246ce781ae3f57e504c48832ecdc515b735
Joseph Corrado <jgc375@nyu.edu>
2019-03-28 19:10:26 -0400

    got the l1xe30 efficiency fits to look nice

---
+++ Mu_Analysis/macros/plot_l1xe30_efficiency_fits.C
@@ -0,0 +1,54 @@
+{
+    TFile* mu_analysis_file = TFile::Open("mu_analysis.root", "READ");
+    TObjArray* l1xe30_efficiency_fit_objects = 0;
+
+    mu_analysis_file->GetObject("l1xe30_efficiency_fit_objects", l1xe30_efficiency_fit_objects);
+    int number_mu_bins = 7;
+    // this needs to be the literal 7 for some reason..
+    int Colors[7] = {1,2,3,4,5,6,9};
+
+    TF1* l1xe30_efficiency_fit;
+    TString outFileName = "";
+    TString Name;
+    TCanvas* l1xe30_eff_canv = new TCanvas("l1xe30_eff_canv","Canvas with L1XE30 Efficiency Fits");
+    l1xe30_eff_canv->SetFillColor(42);
+    TLegend* correctedLegend = new TLegend(0.48,0.2,0.9,0.4);
+    // "NDC" means make limits represent fractions of pad
+    TPaveLabel *title = new TPaveLabel(0.2,0.9,0.8,0.95, "L1XE30 Efficiency Fits", "NDC");
+    // remove showing of random things
+    gStyle->SetOptStat(0);
+    // suppress the title of the first object to be drawn
+    gStyle->SetOptTitle(0);
+    title->SetTextFont(72);
+    l1xe30_efficiency_fit = ((TF1*)(l1xe30_efficiency_fit_objects->At(1)));
+    // don't plot fit functions
+    l1xe30_efficiency_fit->SetLineColor( Colors[1] );
+   Name.Form("L1XE30 Efficiency for %d < #mu < %d" , 10, 20 );
+   l1xe30_efficiency_fit->SetTitle( Name );
+    correctedLegend->AddEntry( l1xe30_efficiency_fit );
+    l1xe30_efficiency_fit->Draw();
+
+    // to fix plotting. draw the mubin 0 first because it was messing everything up
+    for (int i = 2; i <= l1xe30_efficiency_fit_objects->GetLast(); i++){
+        l1xe30_efficiency_fit = ((TF1*)(l1xe30_efficiency_fit_objects->At(i)));
+       Name.Form("L1XE30 Efficiency for %d < #mu < %d" , i*10,(i+1)*10  );
+       l1xe30_efficiency_fit->SetTitle( Name );
+        correctedLegend->AddEntry( l1xe30_efficiency_fit );
+        l1xe30_efficiency_fit->SetLineColor( Colors[i] );
+       l1xe30_efficiency_fit->Draw("SAME");
+    }
+    l1xe30_efficiency_fit = ((TF1*)(l1xe30_efficiency_fit_objects->At(0)));
+   Name.Form("L1XE30 Efficiency for %d < #mu < %d" , 0,10  );
+   l1xe30_efficiency_fit->SetTitle( Name );
+    correctedLegend->AddEntry( l1xe30_efficiency_fit );
+    l1xe30_efficiency_fit->SetLineColor( Colors[0] );
+
+    l1xe30_efficiency_fit->Draw("SAME");
+    correctedLegend->Draw("SAME");
+    l1xe30_eff_canv->SetLogy();
+    title->Draw("SAME");
+
+    outFileName.Form("plots/L1XE30Efficiency_Fits.png");
+    l1xe30_eff_canv->Print(outFileName);
+    mu_analysis_file->Close()
+}
