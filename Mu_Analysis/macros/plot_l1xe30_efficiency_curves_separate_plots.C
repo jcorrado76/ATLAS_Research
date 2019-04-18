6d478a87007a99711d5948dde5bfcf6ce956520a
Joseph Corrado <jgc375@nyu.edu>
2019-04-18 12:06:47 -0400

    moved formatting into src

--- Mu_Analysis/macros/plot_l1xe30_efficiency_curves_separate_plots.C
+++ Mu_Analysis/macros/plot_l1xe30_efficiency_curves_separate_plots.C
@@ -3,14 +3,10 @@
     TObjArray* l1xe30_efficiency_objects = 0;
 
     mu_analysis_file->GetObject("l1xe30_efficiency_objects", l1xe30_efficiency_objects);
-    int number_mu_bins = 7;
-    // this needs to be the literal 7 for some reason..
-    //int Colors[7] = {1,2,3,4,5,6,9};
 
     TEfficiency* l1xe30_efficiency_curve;
     TString outFileName = "";
     TString canvName = "";
-    //TString funcName = "";
 
     for (int i = 0; i <= l1xe30_efficiency_objects->GetLast(); i++){
         canvName.Form("L1XE30 Efficiency for %d < #mu < %d", i*10 , (i+1)*10);
@@ -22,9 +18,6 @@
         gStyle->SetOptTitle(0);
         title->SetTextFont(72);
         l1xe30_efficiency_curve = ((TEfficiency*)(l1xe30_efficiency_objects->At(i)));
-       //funcName.Form("L1XE30 Efficiency for %d < #mu < %d" , i*10, (i+1)*10 );
-       //l1xe30_efficiency_curve->SetTitle( funcName );
-        //l1xe30_efficiency_curve->SetLineColor( Colors[i] );
         l1xe30_efficiency_curve->Draw();
         correctedLegend->AddEntry( l1xe30_efficiency_curve );
         l1xe30_eff_canv->SetLogy();
