a6a091cf1d75e7a2c72fcdc562b36df030212331
Joseph Corrado <jgc375@nyu.edu>
2019-04-18 13:12:56 -0400

    moved all of plotting for corrected to zerobias inside of the reconstructed ploting macro so that I can show thresholds where I took the data from each curve

--- Mu_Analysis/macros/plot_reconstructed.C
+++ Mu_Analysis/macros/plot_reconstructed.C
@@ -7,12 +7,16 @@
     TObjArray* reconstructed_distributions = 0;
     TObjArray* l1xe30_efficiency_objects = 0;
     TObjArray* l1xe50_efficiency_objects = 0;
+    TObjArray* hlt_zb_l1xe30_met_distributions = 0;
+    TObjArray* hlt_zb_l1xe50_met_distributions = 0;
 
     mu_analysis_file->GetObject("hlt_zb_l1zb_met_distributions",hlt_zb_l1_zb_distributions);
     mu_analysis_file->GetObject("hlt_zb_l1xe30_corrected_zb_met_distributions",l1xe30_corrected_zb_distributions);
     mu_analysis_file->GetObject("hlt_zb_l1xe50_corrected_zb_met_distributions",l1xe50_corrected_zb_distributions);
     mu_analysis_file->GetObject("l1xe30_efficiency_objects", l1xe30_efficiency_objects);
     mu_analysis_file->GetObject("l1xe50_efficiency_objects", l1xe50_efficiency_objects);
+    mu_analysis_file->GetObject("hlt_zb_l1xe30__met_distributions",hlt_zb_l1xe30_met_distributions);
+    mu_analysis_file->GetObject("hlt_zb_l1xe50__met_distributions",hlt_zb_l1xe50_met_distributions);
 
     std::cout << "Number of objects in hlt_zb_l1_zb_distributions: " << hlt_zb_l1_zb_distributions->GetEntries() << std::endl;
     TH1D* zb_dist;
@@ -20,6 +24,7 @@
     TH1D* l1xe50_corrected_zb_dist;
     TEfficiency* l1xe30_efficiency_curve;
     TEfficiency* l1xe50_efficiency_curve;
+    TString zb_name, l1xe30_corrected_name, l1xe50_corrected_name ;
 
     Int_t nbins = ((TH1D*)hlt_zb_l1_zb_distributions->At(2))->GetNbinsX();
     std::cout << "Number of MET Bins: " << nbins << std::endl;
@@ -33,24 +38,67 @@
     TString x_axis_label = "Cell Algorithm MET [GeV]";
     TString y_axis_label = "Fraction of events / 5 GeV";
 
+    // reconstruction thresholds
     Int_t L1XE30BinThreshes[7] = {11,11,14,15,15,12,11}; 
     Int_t L1XE50BinThreshes[7] = {20,20,20,20,16,18,18};
 
     TString outFileName = "";
     for (int i = 0; i <= hlt_zb_l1_zb_distributions->GetLast(); i++){
-        // read the histograms into memory
+        // READ IN HISTOGRAMS {{{ 
         zb_dist = ((TH1D*)(hlt_zb_l1_zb_distributions->At(i)));
         l1xe30_corrected_zb_dist = ((TH1D*)(l1xe30_corrected_zb_distributions->At(i)));
         l1xe50_corrected_zb_dist = ((TH1D*)(l1xe50_corrected_zb_distributions->At(i)));
         l1xe30_efficiency_curve = ((TEfficiency*)(l1xe30_efficiency_objects->At(i)));
         l1xe50_efficiency_curve = ((TEfficiency*)(l1xe50_efficiency_objects->At(i)));
-
-        // initialize the new histogram
+        //}}}
+        // MET DISTRIBUTION FORMATTING {{{ 
+        // titles
+        zb_name.Form("Zero Bias Distribution for %d < #mu < %d" , i*10,(i+1)*10  );
+        zb_dist->SetTitle( zb_name );
+        l1xe30_corrected_name.Form("L1XE30 Corrected to ZeroBias Distribution for %d < #mu < %d" , i*10,(i+1)*10  );
+        l1xe30_corrected_zb_dist->SetTitle( l1xe30_corrected_name );
+        l1xe50_corrected_name.Form("L1XE50 Corrected to Zero Bias Distribution for %d < #mu < %d" , i*10,(i+1)*10  );
+        l1xe50_corrected_zb_dist->SetTitle( l1xe50_corrected_name );
+        // set line colors
+        zb_dist->SetLineColor( kBlue );
+        l1xe30_corrected_zb_dist->SetLineColor( kGreen );
+        l1xe50_corrected_zb_dist->SetLineColor( kRed );
+        //}}}
+        // PLOT MET DISTRIBUTIONS WITH EFFICIENCY CURVES {{{
+        TCanvas* corrected_canvas = new TCanvas("correctedCanvas","Canvas with ZB Distributions");
+        gStyle->SetOptStat(0);
+        corrected_canvas->Divide(1,2);
+        corrected_canvas->cd(1);
+        l1xe50_corrected_zb_dist->Draw("P E1");
+        zb_dist->Draw("P E1 SAME");
+        l1xe30_corrected_zb_dist->Draw("P E1 SAME");
+        TLegend* correctedLegend = new TLegend(0.48,0.7,0.9,0.9);
+        correctedLegend->AddEntry( zb_dist );
+        correctedLegend->AddEntry( l1xe30_corrected_zb_dist );
+        correctedLegend->AddEntry( l1xe50_corrected_zb_dist );
+        correctedLegend->Draw("SAME");
+        correctedCanvas->SetLogy();
+        corrected_canvas->cd(2);
+        l1xe30_efficiency_curve->Draw();
+        l1xe50_efficiency_curve->Draw("SAME");
+        auto graph = l1xe30_efficiency_curve->GetPaintedGraph();
+        graph->GetXaxis()->SetRange(0.0,300.0);
+        Double_t l1xe30Discriminant = L1XE30BinThreshes[i] * 5.0;
+        Double_t l1xe50Discriminant = L1XE50BinThreshes[i] * 5.0;
+        TLine* l1xe30Line = new TLine( l1xe30Discriminant , 0.0 , l1xe30Discriminant, 1.0 );
+        TLine* l1xe50Line = new TLine( l1xe50Discriminant , 0.0 , l1xe50Discriminant , 1.0 );
+        l1xe30Line->Draw("SAME");
+        l1xe50Line->Draw("SAME");
+        outFileName.Form("plots/zerobias_distributions_corrected/zb_met_distributions_mubin_%d.png",i);
+        correctedCanvas->Print(outFileName);
+        // }}}
+        // CREATE RECONSTRUCTED HISTOGRAM{{{
         reconstructed_name.Form("Reconstructed Zero Bias Distribution for %d < #mu < %d" , i*10,(i+1)*10  );
         Reconstructed_MET_Distribution->SetTitle(reconstructed_name);
         Reconstructed_MET_Distribution->GetXaxis()->SetTitle(x_axis_label);
         Reconstructed_MET_Distribution->GetYaxis()->SetTitle(y_axis_label);
-
+        //}}}
+        // PERFORM RECONSTRUCTION {{{
         for ( int j = 1 ; j < nbins ; j++ ){
             if (j < L1XE30BinThreshes[i]){
                 Reconstructed_MET_Distribution->SetBinContent( j , zb_dist->GetBinContent( j ) );
@@ -63,8 +111,8 @@
                 Reconstructed_MET_Distribution->SetBinError( j , l1xe50_corrected_zb_dist->GetBinError( j ) );
             }
         }
-
-        // set line color
+        // }}}
+        // PLOT RECONSTRUCTION{{{
         Reconstructed_MET_Distribution->SetLineColor( kBlack );
         Reconstructed_MET_Distribution->SetNormFactor(1.);
 
@@ -82,6 +130,7 @@
         l1xe30_efficiency_curve->Draw();
         l1xe50_efficiency_curve->Draw("SAME");
         auto graph = l1xe30_efficiency_curve->GetPaintedGraph();
+        graph->GetXaxis()->SetRange(0.0,300.0);
         Double_t l1xe30Discriminant = L1XE30BinThreshes[i] * 5.0;
         Double_t l1xe50Discriminant = L1XE50BinThreshes[i] * 5.0;
         TLine* l1xe30Line = new TLine( l1xe30Discriminant , 0.0 , l1xe30Discriminant, 1.0 );
@@ -90,6 +139,7 @@
         l1xe50Line->Draw("SAME");
         outFileName.Form("plots/reconstructed_distributions/reconstructed_distribution_mubin_%d.png",i);
         reconstructed_canvas->Print(outFileName);
+        // }}}
     }
     mu_analysis_file->Close()
 }
