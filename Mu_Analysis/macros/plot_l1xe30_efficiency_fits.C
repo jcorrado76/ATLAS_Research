3a22302776451b4b28bd439fe0869b194dcd4a7d
Joseph Corrado <jgc375@nyu.edu>
2019-04-18 18:49:13 -0400

    finished getting the plots of the reconstructed MET distributions

--- Mu_Analysis/macros/plot_l1xe30_efficiency_fits.C
+++ Mu_Analysis/macros/plot_l1xe30_efficiency_fits.C
@@ -18,18 +18,18 @@
     gStyle->SetOptTitle(0);
     title->SetTextFont(72);
     l1xe30_efficiency_fit = ((TF1*)(l1xe30_efficiency_fit_objects->At(1)));
-   Name.Form("L1XE30 Efficiency Fit for %d < #mu < %d;CELL MET [GeV];#epsilon" , 10, 20 );
-   l1xe30_efficiency_fit->SetTitle( Name );
+    Name.Form("L1XE30 Efficiency Fit for %d < #mu < %d;CELL MET [GeV];#epsilon" , 10, 20 );
+    l1xe30_efficiency_fit->SetTitle( Name );
     correctedLegend->AddEntry( l1xe30_efficiency_fit );
     l1xe30_efficiency_fit->Draw();
 
     // to fix plotting. draw the mubin 0 first because it was messing everything up
     for (int i = 2; i <= l1xe30_efficiency_fit_objects->GetLast(); i++){
         l1xe30_efficiency_fit = ((TF1*)(l1xe30_efficiency_fit_objects->At(i)));
-       Name.Form("L1XE30 Efficiency Fit for %d < #mu < %d;CELL MET [GeV];#epsilon" , i*10,(i+1)*10  );
-       l1xe30_efficiency_fit->SetTitle( Name );
+        Name.Form("L1XE30 Efficiency Fit for %d < #mu < %d;CELL MET [GeV];#epsilon" , i*10,(i+1)*10  );
+        l1xe30_efficiency_fit->SetTitle( Name );
         correctedLegend->AddEntry( l1xe30_efficiency_fit );
-       l1xe30_efficiency_fit->Draw("SAME");
+        l1xe30_efficiency_fit->Draw("SAME");
     }
     l1xe30_efficiency_fit = ((TF1*)(l1xe30_efficiency_fit_objects->At(0)));
    Name.Form("L1XE30 Efficiency Fit for %d < #mu < %d;CELL MET [GeV];#epsilon" , 0,10  );
