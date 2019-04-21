a0bbf92fa4e7a8e15f0b9bff748e4363d029e061
Joseph Corrado <jgc375@nyu.edu>
2019-04-21 09:51:53 -0400

    removed axis labels from title of TF1 because then it shows in the legend. set the axis labels individually after by explicitly setting

--- Mu_Analysis/macros/plot_l1xe50_efficiency_fits.C
+++ Mu_Analysis/macros/plot_l1xe50_efficiency_fits.C
@@ -18,22 +18,24 @@
     gStyle->SetOptTitle(0);
     title->SetTextFont(72);
     l1xe50_efficiency_fit = ((TF1*)(l1xe50_efficiency_fit_objects->At(1)));
-   Name.Form("L1XE50 Efficiency Fit for %d < #mu < %d;CELL MET [GeV];#epsilon" , 10, 20 );
+   Name.Form("L1XE50 Efficiency Fit for %d < #mu < %d" , 10, 20 );
    l1xe50_efficiency_fit->SetTitle( Name );
     correctedLegend->AddEntry( l1xe50_efficiency_fit );
     l1xe50_efficiency_fit->Draw();
+    l1xe50_efficiency_fit->GetHistogram()->GetYaxis()->SetTitle("Efficiency");
+    l1xe50_efficiency_fit->GetHistogram()->GetXaxis()->SetTitle("CELL MET [GeV]");
 
     // to fix plotting. draw the mubin 0 first because it was messing everything up
     for (int i = 2; i <= l1xe50_efficiency_fit_objects->GetLast(); i++){
         l1xe50_efficiency_fit = ((TF1*)(l1xe50_efficiency_fit_objects->At(i)));
-           Name.Form("L1XE50 Efficiency Fit for %d < #mu < %d;CELL MET [GeV];#epsilon" , i*10,(i+1)*10  );
+           Name.Form("L1XE50 Efficiency Fit for %d < #mu < %d" , i*10,(i+1)*10  );
            l1xe50_efficiency_fit->SetTitle( Name );
         correctedLegend->AddEntry( l1xe50_efficiency_fit );
 
         l1xe50_efficiency_fit->Draw("SAME");
     }
     l1xe50_efficiency_fit = ((TF1*)(l1xe50_efficiency_fit_objects->At(0)));
-   Name.Form("L1XE50 Efficiency Fit for %d < #mu < %d;CELL MET [GeV];#epsilon" , 0,10  );
+   Name.Form("L1XE50 Efficiency Fit for %d < #mu < %d" , 0,10  );
    l1xe50_efficiency_fit->SetTitle( Name );
     correctedLegend->AddEntry( l1xe50_efficiency_fit );
 
