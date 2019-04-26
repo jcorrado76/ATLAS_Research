2c345d73b00593d5f7199b0c62e94174e5297d29
Joseph Corrado <jgc375@nyu.edu>
2019-04-26 16:10:29 -0400

    commented and fixed up the compute trigger rate macro

--- HLT_Efficiency_Analysis_Macros/Compute_Trigger_Rate/Compute_Trigger_Rate_TSelector.C
+++ HLT_Efficiency_Analysis_Macros/Compute_Trigger_Rate/Compute_Trigger_Rate_TSelector.C
@@ -1,47 +1,38 @@
-#define Compute_Trigger_Rate_TSelector_cxx
-
+/*
+ * This macro computes what the target trigger rate should be using a cut of L1XE50 and CELL XE 100 on the
+ * zerobias data to simulate triggers currently being used at ATLAS
+ */
 #include "Compute_Trigger_Rate_TSelector.h"
-
-void Compute_Trigger_Rate_TSelector::Begin(TTree * /*tree*/)
+void Compute_Trigger_Rate_TSelector::Begin(TTree*){TString option = GetOption();}
+void Compute_Trigger_Rate_TSelector::SlaveBegin(TTree*)
 {
-   TString option = GetOption();
-}
-
-void Compute_Trigger_Rate_TSelector::SlaveBegin(TTree * /*tree*/)
-{
-    MetL1Hist = new TH1F("h1","Process 2 Histogram", 1200, 0.,300.);
-    RandomHist = new TH1F("h2","Process 1 Histogram", 1200, 0.,300.);
+    Float_t metLo = 0.0;
+    Float_t metHi = 300.0;
+    Int_t nbins = 1200;
+    MetL1Hist = new TH1F("l1hist","L1XE50 Trigger Rate Histogram", nbins , metLo , metHi );
+    RandomHist = new TH1F("zerobiashist","Zerobias Histogram", nbins , metLo , metHi );
     fOutput->Add( MetL1Hist );
     fOutput->Add( RandomHist );
     TString option = GetOption();
 }
-
-
 Bool_t Compute_Trigger_Rate_TSelector::Process(Long64_t entry)
 {
    fReader.SetEntry(entry);
-
     if ( *passrndm > 0.5 )
     {
-        RandomHist->Fill( *metl1 );
-
+        numRndm++;
         if ( *metl1 > 50.0 )
         {
-            MetL1Hist->Fill( *metl1 );
+            numPassL1++;
         }
     }
-
    return kTRUE;
 }
-
-void Compute_Trigger_Rate_TSelector::SlaveTerminate()
-{
-}
-
+void Compute_Trigger_Rate_TSelector::SlaveTerminate(){}
 void Compute_Trigger_Rate_TSelector::Terminate()
 {
-    std::cout << "Number of ZB events: " << RandomHist->GetEntries() << std::endl;
-    std::cout << "Number of events that pass metl1 > 50.0: " << MetL1Hist->GetEntries() << std::endl;
+    std::cout << "Number of ZeroBias events: " << numRndm << std::endl;
+    std::cout << "Number of events that pass metl1 > 50.0: " << numPassL1 << std::endl;
     std::cout << "Trigger rate = Number of events that pass L1>50 / Number Zerobias events: " 
-        << ((MetL1Hist->GetEntries())/(RandomHist->GetEntries())) << std::endl;
+        << numPassL1 / numRndm << std::endl;
 }
--- HLT_Efficiency_Analysis_Macros/include/Compute_Trigger_Rate_TSelector.h
+++ HLT_Efficiency_Analysis_Macros/include/Compute_Trigger_Rate_TSelector.h
@@ -1,13 +1,5 @@
-//////////////////////////////////////////////////////////
-// This class has been automatically generated on
-// Tue Dec 26 21:41:19 2017 by ROOT version 6.13/01
-// from TTree tree/tree
-// found on file: ../../MyData/PhysicsMain.L1KFmuontriggers.2016.f731f758_m1659m1710.Run309759.48Runs.root
-//////////////////////////////////////////////////////////
-
 #ifndef Compute_Trigger_Rate_TSelector_h
 #define Compute_Trigger_Rate_TSelector_h
-
 #include <TH2.h>
 #include <TStyle.h>
 #include <TH1.h>
@@ -18,121 +10,15 @@
 #include <TTreeReader.h>
 #include <TTreeReaderValue.h>
 #include <TTreeReaderArray.h>
-
-// Headers needed by this particular selector
-
-
 class Compute_Trigger_Rate_TSelector : public TSelector {
 public :
-   TTreeReader     fReader;  //!the tree reader
-   TTree          *fChain = 0;   //!pointer to the analyzed TTree or TChain
-    TH1F* MetL1Hist;
-    TH1F* RandomHist;
-
-   // Readers to access the data (delete the ones you do not need){{{
+   TTreeReader     fReader;  
+   TTree          *fChain = 0;   
+   Int_t numRndm = 0;
+   Int_t numPassL1 = 0;
    TTreeReaderValue<Float_t> metl1 = {fReader, "metl1"};
-   TTreeReaderValue<Float_t> mexl1 = {fReader, "mexl1"};
-   TTreeReaderValue<Float_t> meyl1 = {fReader, "meyl1"};
-   TTreeReaderValue<Float_t> setl1 = {fReader, "setl1"};
-   TTreeReaderValue<Int_t> mexl1overflow = {fReader, "mexl1overflow"};
-   TTreeReaderValue<Int_t> meyl1overflow = {fReader, "meyl1overflow"};
-   TTreeReaderValue<Int_t> metl1overflow = {fReader, "metl1overflow"};
-   TTreeReaderValue<Float_t> metl1kf = {fReader, "metl1kf"};
-   TTreeReaderValue<Float_t> mexl1kf = {fReader, "mexl1kf"};
-   TTreeReaderValue<Float_t> meyl1kf = {fReader, "meyl1kf"};
-   TTreeReaderValue<Float_t> setl1kf = {fReader, "setl1kf"};
-   TTreeReaderValue<Float_t> metl1mht = {fReader, "metl1mht"};
-   TTreeReaderValue<Float_t> mexl1mht = {fReader, "mexl1mht"};
-   TTreeReaderValue<Float_t> meyl1mht = {fReader, "meyl1mht"};
-   TTreeReaderValue<Float_t> setl1mht = {fReader, "setl1mht"};
-   TTreeReaderValue<Float_t> metl1mhtkf = {fReader, "metl1mhtkf"};
-   TTreeReaderValue<Float_t> mexl1mhtkf = {fReader, "mexl1mhtkf"};
-   TTreeReaderValue<Float_t> meyl1mhtkf = {fReader, "meyl1mhtkf"};
-   TTreeReaderValue<Float_t> setl1mhtkf = {fReader, "setl1mhtkf"};
-   TTreeReaderValue<Float_t> metl2feb = {fReader, "metl2feb"};
-   TTreeReaderValue<Float_t> mexl2feb = {fReader, "mexl2feb"};
-   TTreeReaderValue<Float_t> meyl2feb = {fReader, "meyl2feb"};
-   TTreeReaderValue<Float_t> setl2feb = {fReader, "setl2feb"};
-   TTreeReaderValue<Float_t> metcell = {fReader, "metcell"};
-   TTreeReaderValue<Float_t> mexcell = {fReader, "mexcell"};
-   TTreeReaderValue<Float_t> meycell = {fReader, "meycell"};
-   TTreeReaderValue<Float_t> setcell = {fReader, "setcell"};
-   TTreeReaderValue<Float_t> mettopocl = {fReader, "mettopocl"};
-   TTreeReaderValue<Float_t> mextopocl = {fReader, "mextopocl"};
-   TTreeReaderValue<Float_t> meytopocl = {fReader, "meytopocl"};
-   TTreeReaderValue<Float_t> settopocl = {fReader, "settopocl"};
-   TTreeReaderValue<Float_t> mextopoclmuon = {fReader, "mextopoclmuon"};
-   TTreeReaderValue<Float_t> meytopoclmuon = {fReader, "meytopoclmuon"};
-   TTreeReaderValue<Float_t> mettopoclem = {fReader, "mettopoclem"};
-   TTreeReaderValue<Float_t> mextopoclem = {fReader, "mextopoclem"};
-   TTreeReaderValue<Float_t> meytopoclem = {fReader, "meytopoclem"};
-   TTreeReaderValue<Float_t> settopoclem = {fReader, "settopoclem"};
-   TTreeReaderValue<Float_t> mettopoclps = {fReader, "mettopoclps"};
-   TTreeReaderValue<Float_t> mextopoclps = {fReader, "mextopoclps"};
-   TTreeReaderValue<Float_t> meytopoclps = {fReader, "meytopoclps"};
-   TTreeReaderValue<Float_t> settopoclps = {fReader, "settopoclps"};
-   TTreeReaderValue<Float_t> mettopoclpuc = {fReader, "mettopoclpuc"};
-   TTreeReaderValue<Float_t> mextopoclpuc = {fReader, "mextopoclpuc"};
-   TTreeReaderValue<Float_t> meytopoclpuc = {fReader, "meytopoclpuc"};
-   TTreeReaderValue<Float_t> settopoclpuc = {fReader, "settopoclpuc"};
-   TTreeReaderValue<Float_t> metmht = {fReader, "metmht"};
-   TTreeReaderValue<Float_t> mexmht = {fReader, "mexmht"};
-   TTreeReaderValue<Float_t> meymht = {fReader, "meymht"};
-   TTreeReaderValue<Float_t> setmht = {fReader, "setmht"};
-   TTreeReaderValue<Float_t> metrefwmuon = {fReader, "metrefwmuon"};
-   TTreeReaderValue<Float_t> mexrefwmuon = {fReader, "mexrefwmuon"};
-   TTreeReaderValue<Float_t> meyrefwmuon = {fReader, "meyrefwmuon"};
-   TTreeReaderValue<Float_t> setrefwmuon = {fReader, "setrefwmuon"};
-   TTreeReaderValue<Float_t> metoffrecal = {fReader, "metoffrecal"};
-   TTreeReaderValue<Float_t> mexoffrecal = {fReader, "mexoffrecal"};
-   TTreeReaderValue<Float_t> meyoffrecal = {fReader, "meyoffrecal"};
-   TTreeReaderValue<Float_t> setoffrecal = {fReader, "setoffrecal"};
-   TTreeReaderValue<Float_t> metrefmuon = {fReader, "metrefmuon"};
-   TTreeReaderValue<Float_t> mexrefmuon = {fReader, "mexrefmuon"};
-   TTreeReaderValue<Float_t> meyrefmuon = {fReader, "meyrefmuon"};
-   TTreeReaderValue<Float_t> metoffrecalmuon = {fReader, "metoffrecalmuon"};
-   TTreeReaderValue<Float_t> mexoffrecalmuon = {fReader, "mexoffrecalmuon"};
-   TTreeReaderValue<Float_t> meyoffrecalmuon = {fReader, "meyoffrecalmuon"};
-   TTreeReaderValue<Float_t> metoffrecaltrksft = {fReader, "metoffrecaltrksft"};
-   TTreeReaderValue<Float_t> mexoffrecaltrksft = {fReader, "mexoffrecaltrksft"};
-   TTreeReaderValue<Float_t> meyoffrecaltrksft = {fReader, "meyoffrecaltrksft"};
-   TTreeReaderValue<Int_t> npv = {fReader, "npv"};
    TTreeReaderValue<Int_t> passrndm = {fReader, "passrndm"};
-   TTreeReaderValue<Int_t> passrndmj40 = {fReader, "passrndmj40"};
-   TTreeReaderValue<Int_t> passmu24med = {fReader, "passmu24med"};
-   TTreeReaderValue<Int_t> passmu24varmed = {fReader, "passmu24varmed"};
-   TTreeReaderValue<Int_t> passmu26med = {fReader, "passmu26med"};
-   TTreeReaderValue<Int_t> passmu26varmed = {fReader, "passmu26varmed"};
-   TTreeReaderValue<Int_t> passnoalgL1XE10 = {fReader, "passnoalgL1XE10"};
-   TTreeReaderValue<Int_t> passnoalgL1XE30 = {fReader, "passnoalgL1XE30"};
-   TTreeReaderValue<Int_t> passnoalgL1XE40 = {fReader, "passnoalgL1XE40"};
-   TTreeReaderValue<Int_t> passnoalgL1XE45 = {fReader, "passnoalgL1XE45"};
-   TTreeReaderValue<Int_t> passnoalgL1XE300 = {fReader, "passnoalgL1XE300"};
-   TTreeReaderValue<Int_t> passxe100 = {fReader, "passxe100"};
-   TTreeReaderValue<Int_t> passxe100mu = {fReader, "passxe100mu"};
-   TTreeReaderValue<Int_t> passxe160tclcw = {fReader, "passxe160tclcw"};
-   TTreeReaderValue<Int_t> passxe160tclcwmu = {fReader, "passxe160tclcwmu"};
-   TTreeReaderValue<Int_t> passxe130tcem = {fReader, "passxe130tcem"};
-   TTreeReaderValue<Int_t> passxe130tcemmu = {fReader, "passxe130tcemmu"};
-   TTreeReaderValue<Int_t> passxe110mht = {fReader, "passxe110mht"};
-   TTreeReaderValue<Int_t> passxe110mhtmu = {fReader, "passxe110mhtmu"};
-   TTreeReaderValue<Int_t> passxe105pufit = {fReader, "passxe105pufit"};
-   TTreeReaderValue<Int_t> passxe110pufit = {fReader, "passxe110pufit"};
-   TTreeReaderValue<Int_t> passxe140pufit = {fReader, "passxe140pufit"};
-   TTreeReaderValue<Int_t> passxe140pufitmu = {fReader, "passxe140pufitmu"};
-   TTreeReaderValue<Int_t> passxe140pueta = {fReader, "passxe140pueta"};
-   TTreeReaderValue<Int_t> passxe140puetamu = {fReader, "passxe140puetamu"};
-   TTreeReaderValue<Int_t> recalbroke = {fReader, "recalbroke"};
-   TTreeReaderValue<Int_t> passcleancuts = {fReader, "passcleancuts"};
-   TTreeReaderValue<Int_t> runnum = {fReader, "runnum"};
-   TTreeReaderValue<Int_t> eventnum = {fReader, "eventnum"};
-   TTreeReaderValue<Int_t> bcid = {fReader, "bcid"};
-   TTreeReaderValue<Float_t> actint = {fReader, "actint"};
-   TTreeReaderValue<Float_t> avint = {fReader, "avint"};
-   //}}}
-
-
-   Compute_Trigger_Rate_TSelector(TTree * /*tree*/ =0) { }
+   Compute_Trigger_Rate_TSelector(TTree * = 0) { }
    virtual ~Compute_Trigger_Rate_TSelector() { }
    virtual Int_t   Version() const { return 2; }
    virtual void    Begin(TTree *tree);
@@ -147,36 +33,10 @@ public :
    virtual TList  *GetOutputList() const { return fOutput; }
    virtual void    SlaveTerminate();
    virtual void    Terminate();
-
    ClassDef(Compute_Trigger_Rate_TSelector,0);
-
 };
-
 #endif
-
 #ifdef Compute_Trigger_Rate_TSelector_cxx
-void Compute_Trigger_Rate_TSelector::Init(TTree *tree)
-{
-   // The Init() function is called when the selector needs to initialize
-   // a new tree or chain. Typically here the reader is initialized.
-   // It is normally not necessary to make changes to the generated
-   // code, but the routine can be extended by the user if needed.
-   // Init() will be called many times when running on PROOF
-   // (once per file to be processed).
-
-   fReader.SetTree(tree);
-}
-
-Bool_t Compute_Trigger_Rate_TSelector::Notify()
-{
-   // The Notify() function is called when a new file is opened. This
-   // can be either for a new TTree in a TChain or when when a new TTree
-   // is started when using PROOF. It is normally not necessary to make changes
-   // to the generated code, but the routine can be extended by the
-   // user if needed. The return value is currently not used.
-
-   return kTRUE;
-}
-
-
+void Compute_Trigger_Rate_TSelector::Init(TTree *tree){fReader.SetTree(tree);}
+Bool_t Compute_Trigger_Rate_TSelector::Notify(){return kTRUE;}
 #endif // #ifdef Compute_Trigger_Rate_TSelector_cxx
